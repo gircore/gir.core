ec966096bb9c76fd0e2a33ca97c6f91ab338f96d
Marcel Tiede <badcel@gmx.de>
2020-03-01 20:54:42 +0100

    Update WebKitGTK

--- Libs/Gtk/Core/Classes/Container.cs
+++ Libs/Gtk/Core/Classes/Container.cs
@@ -6,7 +6,7 @@ namespace Gtk.Core
     public class GContainer : GWidget, Container
     {
         internal GContainer(string template, string obj, Assembly assembly) : base(template, obj, assembly){}
-        internal GContainer(IntPtr handle) : base(handle) {}
+        internal protected GContainer(IntPtr handle) : base(handle) {}
 
         public void Add(GWidget widget) => Gtk.Container.add(this, widget);
     }
--- Libs/WebKitGTK/Core/Classes/WebView.cs
+++ Libs/WebKitGTK/Core/Classes/WebView.cs
@@ -1,7 +1,12 @@
+using Gtk.Core;
+
 namespace WebKitGTK.Core
 {
-    public class WebView
+    public class WebView : GContainer
     {
-        
+        public WebView() : base(WebKit2.WebView.@new())
+        {
+
+        }
     }
 }
