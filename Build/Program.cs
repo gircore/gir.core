165e14f45817d27839cdccec08342e7456461ba4
Marcel Tiede <badcel@gmx.de>
2020-03-29 17:42:40 +0200

    Update

--- Build/Program.cs
+++ Build/Program.cs
@@ -35,6 +35,8 @@ class Program
             project => GenerateProject(project)
         );
 
+        Target(build_gdkpixbuf_core, DependsOn(generate_wrapper), () => DotNetBuild(GDK_PIXBUF_CORE));
+
         Target(build_gtk_core, DependsOn(generate_wrapper), () => DotNetBuild(GTK_CORE));
 
         Target(build_handy_core, DependsOn(build_gtk_core), () => {
@@ -54,7 +56,7 @@ class Program
             DotNetBuild(WEBKIT2WEBEXTENSION_CORE);
         });
 
-        Target(build_core_projects, DependsOn(build_handy_core, build_gtk_core, build_webkitgtk_core, build_webkit2webextensions_core));
+        Target(build_core_projects, DependsOn(build_gdkpixbuf_core, build_handy_core, build_gtk_core, build_webkitgtk_core, build_webkit2webextensions_core));
 
         Target(publish, DependsOn(use_release_configuration, build_core_projects),
             ForEach(
@@ -68,6 +70,7 @@ class Program
                 GDK_PIXBUF_WRAPPER,
                 GTK_WRAPPER,
                 GOBJECT_CORE,
+                GDK_PIXBUF_CORE,
                 GLIB_CORE,
                 GIO_CORE,
                 GTK_CORE,
@@ -78,6 +81,27 @@ class Program
                 DotnetPack(project);
                 DotNetNugetPush(project);
         });
+
+        Target(clean, ForEach(GLIB_WRAPPER,
+                CAIRO_WRAPPER,
+                XLIB_WRAPPER,
+                PANGO_WRAPPER,
+                GDK_WRAPPER,
+                GIO_WRAPPER,
+                GOBJECT_WRAPPER,
+                GDK_PIXBUF_WRAPPER,
+                GTK_WRAPPER,
+                GOBJECT_CORE,
+                GDK_PIXBUF_CORE,
+                GLIB_CORE,
+                GIO_CORE,
+                GTK_CORE,
+                HANDY_CORE,
+                WEBKITGTK_CORE,
+                JAVASCRIPT_CORE_CORE),
+            (project) => DotNetClean(project));
+
+        Target(clean_release, DependsOn(use_release_configuration, clean));
 
         Target("default", DependsOn(build_core_projects));
         RunTargetsAndExit(args);
@@ -90,12 +114,13 @@ class Program
     };
 
     private static void DotNetNugetPush(string project) => Run(dotnet, $@"{nuget} {push} ""bin/{configuration}/*.nupkg"" -s https://api.nuget.org/v3/index.json -k {NUGET_API_KEY} --skip-duplicate", project);
-    private static void DotnetPack(string project) => Run(dotnet, $"{pack} --nologo -c {configuration}", project);
+    private static void DotnetPack(string project) => Run(dotnet, $"{pack} --nologo --no-build -c {configuration}", project);
     private static void DotNetBuild(string project) {
         Run(dotnet,$"{restore}{GetDisableParallel()} {project}");
         Run(dotnet, $"{build} --no-restore --nologo -c {configuration}{GetDisableParallel()} {project}");
     } 
     private static void DotNetRun(string project) => Run(dotnet, $"{run} -c {configuration} {project}", project);
+    private static void DotNetClean(string project) => Run(dotnet, $"{clean} --nologo -c {configuration}", project);
 
     private static void GenerateProject(string path)
     {
--- Build/Targets.cs
+++ Build/Targets.cs
@@ -6,7 +6,10 @@ static class Targets
     public const string build_gtk_core = "build-gtk-core";
     public const string build_handy_core = "build-handy-core";
     public const string build_webkitgtk_core = "build-webkitgtk-core";
+    public const string build_gdkpixbuf_core = "build-gdkpixbuf-core";
     public const string build_webkit2webextensions_core = "build-webkit2webextensions-core";
     public const string build_core_projects = "build-core-projects";
     public const string publish = "publish";
+    public const string clean = "clean";
+    public const string clean_release = "clean-release";
 }
