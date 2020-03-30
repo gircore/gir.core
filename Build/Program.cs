074859e38d206ba79317d4c0cb9254b8884820c6
Marcel Tiede <badcel@gmx.de>
2020-03-30 20:01:59 +0200

    Simplify relase

--- Build/Program.cs
+++ Build/Program.cs
@@ -3,23 +3,15 @@ using static SimpleExec.Command;
 using static Targets;
 using static Commands;
 using static Projects;
-using System;
 
 class Program
 {
-    private static string configuration = "Debug";
-    private static string? NUGET_API_KEY = "";
-    private const string release = "Release";
+    private static string configuration = confDebug;
+    private const string confRelease = "Release";
+    private const string confDebug = "Debug";
 
     static void Main(string[] args)
     {
-        Environment.SetEnvironmentVariable("MSBUILDSINGLELOADCONTEXT", "1");
-        NUGET_API_KEY = Environment.GetEnvironmentVariable("NUGET_API_KEY");
-
-        Target(use_release_configuration, () =>{
-            configuration = release;
-        });
-
         Target(generate_wrapper, 
             ForEach(
                 CWRAPPER, 
@@ -56,31 +48,7 @@ class Program
             DotNetBuild(WEBKIT2WEBEXTENSION_CORE);
         });
 
-        Target(build_core_projects, DependsOn(build_gdkpixbuf_core, build_handy_core, build_gtk_core, build_webkitgtk_core, build_webkit2webextensions_core));
-
-        Target(publish, DependsOn(use_release_configuration, build_core_projects),
-            ForEach(
-                GLIB_WRAPPER,
-                CAIRO_WRAPPER,
-                XLIB_WRAPPER,
-                PANGO_WRAPPER,
-                GDK_WRAPPER,
-                GIO_WRAPPER,
-                GOBJECT_WRAPPER,
-                GDK_PIXBUF_WRAPPER,
-                GTK_WRAPPER,
-                GOBJECT_CORE,
-                GDK_PIXBUF_CORE,
-                GLIB_CORE,
-                GIO_CORE,
-                GTK_CORE,
-                HANDY_CORE,
-                WEBKITGTK_CORE,
-                JAVASCRIPT_CORE_CORE), 
-            (project) => {
-                DotnetPack(project);
-                DotNetNugetPush(project);
-        });
+        Target(Targets.build, DependsOn(build_gdkpixbuf_core, build_handy_core, build_gtk_core, build_webkitgtk_core, build_webkit2webextensions_core));
 
         Target(clean, ForEach(GLIB_WRAPPER,
                 CAIRO_WRAPPER,
@@ -100,24 +68,17 @@ class Program
                 WEBKITGTK_CORE,
                 JAVASCRIPT_CORE_CORE),
             (project) => DotNetClean(project));
+        
+        Target(release, () => configuration = confRelease);
+        Target(debug, () => configuration = confDebug);
 
-        Target(clean_release, DependsOn(use_release_configuration, clean));
-
-        Target("default", DependsOn(build_core_projects));
+        Target("default", DependsOn(Targets.build));
         RunTargetsAndExit(args);
     }
 
-    private static string GetDisableParallel() => configuration switch
-    {
-        release => " --disable-parallel",
-        _ => ""
-    };
-
-    private static void DotNetNugetPush(string project) => Run(dotnet, $@"{nuget} {push} ""bin/{configuration}/*.nupkg"" -s https://api.nuget.org/v3/index.json -k {NUGET_API_KEY} --skip-duplicate", project);
-    private static void DotnetPack(string project) => Run(dotnet, $"{pack} --nologo --no-build -c {configuration}", project);
     private static void DotNetBuild(string project) {
-        Run(dotnet,$"{restore}{GetDisableParallel()} {project}");
-        Run(dotnet, $"{build} --no-restore --nologo -c {configuration}{GetDisableParallel()} {project}");
+        Run(dotnet,$"{restore} {project}");
+        Run(dotnet, $"{Commands.build} --no-restore --nologo -c {configuration} {project}");
     } 
     private static void DotNetRun(string project) => Run(dotnet, $"{run} -c {configuration} {project}", project);
     private static void DotNetClean(string project) => Run(dotnet, $"{clean} --nologo -c {configuration}", project);
--- Build/Targets.cs
+++ Build/Targets.cs
@@ -1,15 +1,13 @@
 static class Targets
 {
-    public const string use_release_configuration = "use-release-configuration";
     public const string generate_wrapper = "generate-wrapper";
-    public const string build_gtk_wrapper = "build-gtk-wrapper";
     public const string build_gtk_core = "build-gtk-core";
     public const string build_handy_core = "build-handy-core";
     public const string build_webkitgtk_core = "build-webkitgtk-core";
     public const string build_gdkpixbuf_core = "build-gdkpixbuf-core";
     public const string build_webkit2webextensions_core = "build-webkit2webextensions-core";
-    public const string build_core_projects = "build-core-projects";
-    public const string publish = "publish";
+    public const string build = "build";
     public const string clean = "clean";
-    public const string clean_release = "clean-release";
+    public const string debug = "debug";
+    public const string release = "release";
 }
