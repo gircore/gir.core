a2c23238c456d6e3ffc26bba4d2cc729069cea10
Marcel Tiede <badcel@gmx.de>
2020-06-14 21:24:37 +0200

    Simplify build

--- Build/Targets.cs
+++ Build/Targets.cs
@@ -1,13 +1,6 @@
 static class Targets
 {
     public const string generate_wrapper = "generate-wrapper";
-    public const string build_gtk_core = "build-gtk-core";
-    public const string build_handy_core = "build-handy-core";
-    public const string build_webkitgtk_core = "build-webkitgtk-core";
-    public const string build_gdkpixbuf_core = "build-gdkpixbuf-core";
-    public const string build_webkit2webextensions_core = "build-webkit2webextensions-core";
-    public const string build_gtkchamplain_core = "build-gtkchamplain-core";
-    public const string build_gtkclutter_core = "build-gtkclutter-core";
     public const string build = "build";
     public const string clean = "clean";
     public const string debug = "debug";
