e2d4d9c9adc8c4b7c934e92999ffede18e75b029
Marcel Tiede <badcel@gmx.de>
2020-06-18 22:38:17 +0200

    Update

--- Libs/Gst/Core/Classes/Bus.cs
+++ Libs/Gst/Core/Classes/Bus.cs
@@ -4,12 +4,11 @@ namespace Gir.Core.Gst
 {
     public class Bus : GObject.Core.GObject
     {
-        protected internal Bus(IntPtr handle) : base(handle, true)
-        {
-        }
+        protected internal Bus(IntPtr handle) : base(handle, true) { }
 
         public void WaitForEndOrError()
             => TimedPopFiltered(global::Gst.Constants.CLOCK_TIME_NONE);
+            
         public void TimedPopFiltered(ulong timeout)
             => global::Gst.Bus.timed_pop_filtered(this, timeout, (global::Gst.MessageType) (MessageType.EOS | MessageType.Error));
     }
