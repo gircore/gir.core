a33c350b4599f27a2f274a753af5cc360b801655
Marcel Tiede <marcel@Obelix.lan>
2020-07-24 07:08:09 +0200

    Add GTK4 wrapper

--- Build/Program.cs
+++ Build/Program.cs
@@ -17,25 +17,24 @@ class Program
         XLIB_WRAPPER,
         PANGO_WRAPPER,
         GDK3_WRAPPER,
-        GDK4_WRAPPER,
         GIO_WRAPPER,
         GOBJECT_WRAPPER,
         GDK_PIXBUF_WRAPPER,
-        GSK4_WRAPPER,
         GTK3_WRAPPER,
-        GTK4_WRAPPER,
         WEBKIT2WEBEXTENSION_WRAPPER,
         CLUTTER_WRAPPER,
         GTKCLUTTER_WRAPPER,
         CHAMPLAIN_WRAPPER,
         GTKCHAMPLAIN_WRAPPER,
         GST_WRAPPER,
+        GDK4_WRAPPER, //GTK4
+        GSK4_WRAPPER, //GTK4
+        GTK4_WRAPPER, //GTK4
         GOBJECT_CORE,
         GDK_PIXBUF_CORE,
         GLIB_CORE,
         GIO_CORE,
         GTK3_CORE,
-        /*GTK4_CORE,*/
         HANDY_CORE,
         WEBKITGTK_CORE,
         JAVASCRIPT_CORE_CORE,
@@ -58,11 +57,8 @@ class Program
                 (XLIB_WRAPPER, "xlib-2.0.gir", "TODO", false),
                 (PANGO_WRAPPER, "Pango-1.0.gir", "TODO", false),
                 (GDK3_WRAPPER, "Gdk-3.0.gir", "TODO", true),
-                (GDK4_WRAPPER, "Gdk-4.0.gir", "libgtk-4.so.0", true),
                 (GDK_PIXBUF_WRAPPER, "GdkPixbuf-2.0.gir", "libgdk_pixbuf-2.0.so.0", true),
                 (GTK3_WRAPPER, "Gtk-3.0.gir", "libgtk-3.so.0", true),
-                (GSK4_WRAPPER, "Gsk-4.0.gir", "libgtk-4.so.0", true),
-                (GTK4_WRAPPER, "Gtk-4.0.gir", "libgtk-4.so.0", true),
                 (JAVASCRIPT_CORE_WRAPPER, "JavaScriptCore-4.0.gir", "javascriptcoregtk-4.0.so", false),
                 (HANDY_WRAPPER, "Handy-0.0.gir", "libhandy-0.0.so.0", false),
                 (WEBKITGTK_WRAPPER, "WebKit2-4.0.gir", "libwebkit2gtk-4.0.so.37", true),
@@ -71,7 +67,10 @@ class Program
                 (GTKCLUTTER_WRAPPER, "GtkClutter-1.0.gir", "libclutter-gtk-1.0.so.0", false),
                 (CHAMPLAIN_WRAPPER, "Champlain-0.12.gir", "libchamplain-0.12", false),
                 (GTKCHAMPLAIN_WRAPPER, "GtkChamplain-0.12.gir", "libchamplain-gtk-0.12.so.0", false),
-                (GST_WRAPPER, "Gst-1.0.gir", "libgstreamer-1.0.so.0", true)
+                (GST_WRAPPER, "Gst-1.0.gir", "libgstreamer-1.0.so.0", true),
+                (GDK4_WRAPPER, "Gdk-4.0.gir", "libgtk-4.so.0", true),//GTK4
+                (GSK4_WRAPPER, "Gsk-4.0.gir", "libgtk-4.so.0", true),//GTK4
+                (GTK4_WRAPPER, "Gtk-4.0.gir", "libgtk-4.so.0", true) //GTK4
                 ),
             (x) => Generate(x.project, x.girFile, x.import, x.addAlias)
         );
