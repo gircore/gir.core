ee7b2219cc92d1380bd63748071e233725b74797
Nana Axel <ax.lnana@outlook.com>
2020-08-24 09:37:07 +0100

    Make GObject internal API visible to other parts of Gir.Core
    Only Gtk3.Core for now.

---
+++ Libs/GObject/Core/Properties/AssemblyVisibility.cs
@@ -0,0 +1,6 @@
+using System.Runtime.CompilerServices;
+
+// Makes internals API available to other Gir.Core
+// libraries, without giving access to the end user.
+
+[assembly: InternalsVisibleTo("Gtk3.Core")]
