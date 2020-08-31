432539a475b3b25a9fcc9e20c631aeaa5d1719b1
Nana Axel <ax.lnana@outlook.com>
2020-08-31 06:28:44 +0100

    Cleanup the Binding class

--- Libs/GObject/Core/Classes/Binding.cs
+++ Libs/GObject/Core/Classes/Binding.cs
@@ -4,63 +4,63 @@ namespace GObject
 {
     public class Binding : Object
     {
-        private static readonly TypeDescriptor GTypeDescriptor = TypeDescriptor.For("GBinding", Sys.Binding.get_type);
-
         #region Properties
 
-        public static readonly Property<Object> SourcePropertyDep = GObject.Property<Object>.Register<Binding>(
+        private static readonly TypeDescriptor GTypeDescriptor = TypeDescriptor.For("GBinding", Sys.Binding.get_type);
+
+        public static readonly Property<Object> SourceProperty = Property<Object>.Register<Binding>(
             "source",
-            nameof(SourceEx),
-            (o) => o.SourceEx,
-            (o, v) => o.SourceEx = v
+            nameof(Source),
+            (o) => o.Source,
+            (o, v) => o.Source = v
         );
 
-        public Object SourceEx
+        public Object Source
         {
-            get => GetProperty(SourcePropertyDep);
-            set => SetProperty(SourcePropertyDep, value);
+            get => GetProperty(SourceProperty);
+            set => SetProperty(SourceProperty, value);
         }
 
-        public static readonly Property<string> SourcePropertyPropertyDep = GObject.Property<string>.Register<Binding>(
+        public static readonly Property<string> SourcePropertyProperty = Property<string>.Register<Binding>(
             "source-property",
-            nameof(SourcePropertyEx),
-            (o) => o.SourcePropertyEx,
-            (o, v) => o.SourcePropertyEx = v
+            nameof(SourcePropertyName),
+            (o) => o.SourcePropertyName,
+            (o, v) => o.SourcePropertyName = v
         );
 
-        public string SourcePropertyEx
+        public string SourcePropertyName
         {
-            get => GetProperty(SourcePropertyPropertyDep);
-            set => SetProperty(SourcePropertyPropertyDep, value);
+            get => GetProperty(SourcePropertyProperty);
+            set => SetProperty(SourcePropertyProperty, value);
         }
 
-        public static readonly Property<Object> TargetPropertyDep = GObject.Property<Object>.Register<Binding>(
+        public static readonly Property<Object> TargetProperty = Property<Object>.Register<Binding>(
             "target",
-            nameof(TargetEx),
-            (o) => o.TargetEx,
-            (o, v) => o.TargetEx = v
+            nameof(Target),
+            (o) => o.Target,
+            (o, v) => o.Target = v
         );
 
-        public Object TargetEx
+        public Object Target
         {
-            get => GetProperty(TargetPropertyDep);
-            set => SetProperty(TargetPropertyDep, value);
+            get => GetProperty(TargetProperty);
+            set => SetProperty(TargetProperty, value);
         }
 
-        public static readonly Property<string> TargetPropertyPropertyDep = GObject.Property<string>.Register<Binding>(
+        public static readonly Property<string> TargetPropertyProperty = Property<string>.Register<Binding>(
             "target-property",
-            nameof(TargetPropertyEx),
-            (o) => o.TargetPropertyEx,
-            (o, v) => o.TargetPropertyEx = v
+            nameof(TargetPropertyName),
+            (o) => o.TargetPropertyName,
+            (o, v) => o.TargetPropertyName = v
         );
 
-        public string TargetPropertyEx
+        public string TargetPropertyName
         {
-            get => GetProperty(TargetPropertyPropertyDep);
-            set => SetProperty(TargetPropertyPropertyDep, value);
+            get => GetProperty(TargetPropertyProperty);
+            set => SetProperty(TargetPropertyProperty, value);
         }
 
-        public static readonly Property<Sys.BindingFlags> FlagsProperty = GObject.Property<Sys.BindingFlags>.Register<Binding>(
+        public static readonly Property<Sys.BindingFlags> FlagsProperty = Property<Sys.BindingFlags>.Register<Binding>(
             "flags",
             nameof(Flags),
             (o) => o.Flags,
@@ -73,33 +73,18 @@ namespace GObject
             set => SetProperty(FlagsProperty, value);
         }
 
-        private const string propSource = "source";
-        public Object Source => GetObject<Object>(propSource);
-
-        private const string propSourceProperty = "source-property";
-        public string SourceProperty => GetStr(propSourceProperty);
-
-        private const string propTarget = "target";
-        public Object Target => GetObject<Object>(propTarget);
-
-        private const string propTargetProperty = "target-property";
-        public string TargetProperty => GetStr(propTargetProperty);
-
-        private const string propFlags = "flags";
         #endregion Properties
 
         internal Binding(IntPtr handle) : base(handle) { }
 
         public Binding(Object source, string sourceProperty, Object target, string targetProperty)
             : base(
-                ConstructProp.With(SourcePropertyDep, source),
-                ConstructProp.With(SourcePropertyPropertyDep, sourceProperty),
-                ConstructProp.With(TargetPropertyDep, target),
-                ConstructProp.With(TargetPropertyPropertyDep, targetProperty),
+                ConstructProp.With(SourceProperty, source),
+                ConstructProp.With(SourcePropertyProperty, sourceProperty),
+                ConstructProp.With(TargetProperty, target),
+                ConstructProp.With(TargetPropertyProperty, targetProperty),
                 ConstructProp.With(FlagsProperty, Sys.BindingFlags.bidirectional))
-        {
-
-        }
+        { }
 
         protected override void Dispose(bool disposing)
         {
