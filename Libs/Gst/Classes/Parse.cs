e6395adb2f81c0987003b57d90eff3df721c7dda
badcel <1218031+badcel@users.noreply.github.com>
2020-10-29 17:33:09 +0100

    Gst.Parse: Add error handling

--- Libs/Gst/Classes/Parse.cs
+++ Libs/Gst/Classes/Parse.cs
@@ -2,14 +2,21 @@ using System;
 
 namespace Gst
 {
-    public class Parse
+    public static class Parse
     {
+        /// <summary>
+        /// Creates a new pipeline.
+        /// </summary>
+        /// <param name="pipelineDescription">Description of the pipeline</param>
+        /// <returns>A new element</returns>
+        /// <exception cref="GException">Throws an exception in case of an error</exception>
         public static Element Launch(string pipelineDescription)
         {
             IntPtr ret = Global.parse_launch(pipelineDescription, out IntPtr error);
 
-            //TODO: Handle error
-            
+            if (error != IntPtr.Zero)
+                throw new GLib.GException(error);
+
             return new Element(ret); //Parse always returns a new object
         }
     }
