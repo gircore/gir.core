6d561c8e33c832befc7004189939489e1f7de548
badcel <1218031+badcel@users.noreply.github.com>
2020-10-31 15:24:57 +0100

    Global: Move native methods into nested internal class  - Fixes #91

--- Libs/Gst/Classes/Application.Static.cs
+++ Libs/Gst/Classes/Application.Static.cs
@@ -9,7 +9,7 @@ namespace Gst
             var argc = 0;
             IntPtr argv = IntPtr.Zero;
 
-            Global.init(ref argc, ref argv);
+            Global.Native.init(ref argc, ref argv);
         }
     }
 }
