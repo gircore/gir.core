0b5064e773450b029dd0d44d95485ac712479655
badcel <1218031+badcel@users.noreply.github.com>
2020-11-11 20:00:46 +0100

    Add AssemblyExtension to read resources

---
+++ Libs/GObject/Extensions/AssemblyExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using GLib;
+
+namespace GObject
+{
+    public static class AssemblyExtension
+    {
+        public static Bytes ReadResource(this Assembly assembly, string template)
+        {
+            Stream? stream = assembly.GetManifestResourceStream(template);
+
+            if (stream == null)
+                throw new Exception("Cannot get resource file '" + template + "'");
+
+            var size = (int) stream.Length;
+            var buffer = new byte[size];
+            stream.Read(buffer, 0, size);
+            stream.Close();
+
+            return Bytes.From(buffer);
+        }
+    }
+}
