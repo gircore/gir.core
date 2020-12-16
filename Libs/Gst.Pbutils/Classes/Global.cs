4655d953a6b22efffdf4bc97902acf4cce980962
Matthew Jakeman <mjak923@aucklanduni.ac.nz>
2020-12-16 18:43:06 +1300

    Remove non-functioning code in InstallPluginsAsync

--- Libs/Gst.Pbutils/Classes/Global.cs
+++ Libs/Gst.Pbutils/Classes/Global.cs
@@ -50,44 +50,14 @@ namespace GstPbutils
             return Marshal.PtrToStringAnsi(result);
         }
 
-        // TODO: Rework with C# async/await
-        // TODO: Rework. None of this works
         public static InstallPluginsReturn InstallPluginsAsync(string[] details, InstallPluginsContext? context,
             InstallPluginsResultFunc func)
         {
-            throw new NotImplementedException("This Function is not implemented");
-            
             // FIXME: We currently cannot support InstallPluginsContext because it is an opaque/disguised struct
             // that is for some reason being generated as a struct. Therefore, marshalling will have no effect
             // (because there are no fields) and the entire thing will simply blow up. Fix this ASAP.
-            /*if (context != null)
-                throw new NotImplementedException("Install Contexts are not currently supported!");
+            throw new NotImplementedException("This Function is not implemented");
             
-            // Marshal this structure
-            // IntPtr contextPtr = Marshal.AllocHGlobal(Marshal.SizeOf(context));
-            // Marshal.StructureToPtr(context!, contextPtr, false);
-
-            // We have properties
-            // Prepare Construct Properties
-            var detailsArray = new IntPtr[details.Length];
-
-            // Populate arrays
-            for (var i = 0; i < details.Length; i++)
-            {
-                // TODO: Marshal in a block, rather than one at a time
-                // for performance reasons. ADD SOME KIND OF FUNCTION
-                // THAT DOES THIS RELIABLY FOR US!
-                detailsArray[i] = Marshal.StringToHGlobalAnsi(details[i]);
-            }
-
-            // Native Call
-            InstallPluginsReturn result = Native.install_plugins_async(detailsArray[0], /*contextPtr#1# IntPtr.Zero, func, IntPtr.Zero);
-            
-            // Free strings
-            foreach (IntPtr ptr in detailsArray)
-                Marshal.FreeHGlobal(ptr);
-
-            return result;*/
+            // We will want to also find some way of dealing with async/await for Asynchronous GLib methods.
         }
-    }
 }
