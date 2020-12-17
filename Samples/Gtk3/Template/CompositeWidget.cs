95ae62ce940bbd8be9f19fbda27c84076a3797fe
badcel <1218031+badcel@users.noreply.github.com>
2020-12-17 15:47:13 +0100

    Composite templates: Add sample application

---
+++ Samples/Gtk3/Template/CompositeWidget.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using GObject;
+using Gtk;
+using Type = GObject.Type;
+
+namespace GtkDemo
+{
+    public class CompositeWidget : Bin
+    {
+        private static void ClassInit(Type gClass, System.Type type, IntPtr classData)
+        {
+            SetTemplate(
+                gtype: gClass, 
+                template: Assembly.GetExecutingAssembly().ReadResource("CompositeWidget.glade")
+            );
+            BindTemplateChild(gClass, nameof(Button));
+            OnConnectEvent(gClass, type);
+        }
+
+        protected override void Initialize()
+        {
+            InitTemplate();
+            BindTemplateChild(nameof(Button), ref Button);
+        }
+
+        private Button Button = default!;
+
+        private void button_clicked(Button sender, System.EventArgs args)
+        {
+            sender.Label = "Clicked!";
+        }
+    }
+}
