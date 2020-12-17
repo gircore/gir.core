073e850288744476e08efff075611f4e65be89c6
badcel <1218031+badcel@users.noreply.github.com>
2020-12-17 20:25:49 +0100

    Add Integrator project

---
+++ Integrator/Integrator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Integrator
+{
+    [Generator]
+    public class Integrator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
