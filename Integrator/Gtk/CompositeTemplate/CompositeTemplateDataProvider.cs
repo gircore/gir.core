3cb042e646ece2e19419425ff6458aa8d4054b68
badcel <1218031+badcel@users.noreply.github.com>
2020-12-19 11:49:32 +0100

    CompositeTemplateDataProvider: Refactor

--- Integrator/Gtk/CompositeTemplate/CompositeTemplateDataProvider.cs
+++ Integrator/Gtk/CompositeTemplate/CompositeTemplateDataProvider.cs
@@ -8,14 +8,24 @@ namespace Integrator.Gtk
     {
         private readonly ClassDeclarationSyntax _classDeclarationSyntax;
 
+        #region Properties
+        
         public string ClassName => GetClassName();
         public string TemplateName => GetTemplateName();
+        
+        #endregion
+        
+        #region Constructors
         
         public CompositeTemplateDataProvider(ClassDeclarationSyntax classDeclarationSyntax)
         {
             _classDeclarationSyntax = classDeclarationSyntax ?? throw new ArgumentNullException(nameof(classDeclarationSyntax));
         }
 
+        #endregion
+        
+        #region Methods
+        
         private string GetClassName()
         {
             return _classDeclarationSyntax.Identifier.ValueText;
@@ -23,10 +33,28 @@ namespace Integrator.Gtk
 
         private string GetTemplateName()
         {
-            var attributes = _classDeclarationSyntax.AttributeLists.SelectMany(x => x.Attributes);
-            var attribute = attributes.First(x => x.Name.ToString() == "Template");
+            var attribute = GetClassAttribute("Template");
+            var firstArgument = GetFirstArgument(attribute);
+            
+            return firstArgument;
+        }
 
+        private AttributeSyntax GetClassAttribute(string attributeName)
+        {
+            var attributes = _classDeclarationSyntax.AttributeLists.SelectMany(x => x.Attributes);
+            var attribute = attributes.First(x => x.Name.ToString() == attributeName);
+
+            return attribute;
+        }
+        
+        private string GetFirstArgument(AttributeSyntax attribute)
+        {
+            if (attribute.ArgumentList is null)
+                throw new Exception($"Class {ClassName} does not contain a \"Template\" attribute with arguments.");
+            
             return attribute.ArgumentList.Arguments.First().ToString();
         }
+        
+        #endregion
     }
 }
