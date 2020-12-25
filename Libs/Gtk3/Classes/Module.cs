92eee5b56a2db4e9d47e437e443095c7ee778575
badcel <1218031+badcel@users.noreply.github.com>
2020-12-25 07:42:55 +0100

    Add module initializers to libs

---
+++ Libs/Gtk3/Classes/Module.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Gtk
+{
+    internal partial class Module
+    {
+        [ModuleInitializer]
+        internal static void Initialize()
+        {
+            InitializeDllImport();
+        }
+
+        static partial void InitializeDllImport();
+    }
+}
