5c58eeb25c75e4ea24aa8a73993eb1454d87ca8a
badcel <1218031+badcel@users.noreply.github.com>
2021-01-03 09:57:27 +0100

    GLib: Add helper to avoid memory leaks

---
+++ Libs/GLib/Classes/MarshalHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GLib
+{
+    public class MarshalHelper
+    {
+        public static K Execute<T, K>(T s, Func<IntPtr, K> action)  where T : struct
+        {
+            K result;
+            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(s));
+            try
+            {
+                Marshal.StructureToPtr(s, ptr, true);
+                result = action(ptr);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(ptr);
+            }
+
+            return result;
+        }
+        
+        public static void Execute<T>(T s, Action<IntPtr> action) where T : struct
+        {
+            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(s));
+            try
+            {
+                Marshal.StructureToPtr(s, ptr, true);
+                action(ptr);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(ptr);
+            }
+        }
+    }
+}
---
+++ Libs/GLib/Extensions/IntPtrExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GLib
+{
+    public static class IntPtrExtension
+    {
+        public static string ToAnsiStringAndFree(this IntPtr ptr)
+        {
+            var resultString = Marshal.PtrToStringAnsi(ptr);
+            Global.Native.free(ptr);
+            return resultString ?? "";
+        }
+    }
+}
