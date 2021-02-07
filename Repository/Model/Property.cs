9efeebbe80dc31210342a5bc7cbe27d52cd818d7
badcel <1218031+badcel@users.noreply.github.com>
2021-02-07 06:54:17 +0100

    Model: Add property

---
+++ Repository/Model/Property.cs
@@ -0,0 +1,18 @@
+﻿using Repository.Analysis;
+
+namespace Repository.Model
+{
+    public class Property : Symbol
+    {
+        public Transfer Transfer { get; }
+        public bool Writeable { get; }
+        public ISymbolReference SymbolReference { get; }
+        
+        public Property(string nativeName, string managedName, ISymbolReference symbolReference, bool writeable, Transfer transfer) : base(nativeName, managedName)
+        {
+            SymbolReference = symbolReference;
+            Writeable = writeable;
+            Transfer = transfer;
+        }
+    }
+}
