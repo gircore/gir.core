62b291095c9a69f11fb3107065b55c006076f3b9
badcel <1218031+badcel@users.noreply.github.com>
2021-02-17 17:01:19 +0100

    Repository: Class model should not inherit from interface model
    There are elements inside an interface which are not part of a class
    - Element: Prerequesite
    - See: https://gitlab.gnome.org/GNOME/gobject-introspection/blob/master/docs/gir-1.2.rnc)

--- Repository/Model/Class.cs
+++ Repository/Model/Class.cs
@@ -4,25 +4,39 @@ using Repository.Analysis;
 
 namespace Repository.Model
 {
-    public class Class : Interface
+    public class Class : Type
     {
+        public string CType { get; }
+        public Method GetTypeFunction { get; }
+        public IEnumerable<ISymbolReference> Implements { get; }
+        
+        public IEnumerable<Method> Methods { get; }
+        public IEnumerable<Method> Functions { get; }
         public ISymbolReference? Parent { get; }
         public IEnumerable<Property> Properties { get; }
         public IEnumerable<Field> Fields { get; }
         public IEnumerable<Signal> Signals { get; }
 
-        public Class(Namespace @namespace, string nativeName, string managedName, string ctype, ISymbolReference? parent, IEnumerable<ISymbolReference> implements, IEnumerable<Method> methods, IEnumerable<Method> functions, Method getTypeFunction, IEnumerable<Property> properties, IEnumerable<Field> fields, IEnumerable<Signal> signals) : base(@namespace, nativeName, managedName, ctype, implements, methods, functions, getTypeFunction)
+        public Class(Namespace @namespace, string nativeName, string managedName, string ctype, ISymbolReference? parent, IEnumerable<ISymbolReference> implements, IEnumerable<Method> methods, IEnumerable<Method> functions, Method getTypeFunction, IEnumerable<Property> properties, IEnumerable<Field> fields, IEnumerable<Signal> signals) : base(@namespace, nativeName, managedName)
         {
             Parent = parent;
+            Implements = implements;
+            Methods = methods;
+            Functions = functions;
+            GetTypeFunction = getTypeFunction;
             Properties = properties;
             Fields = fields;
             Signals = signals;
+            CType = ctype;
         }
 
         public override IEnumerable<ISymbolReference> GetSymbolReferences()
         {
             var symbolReferences = IEnumerables.Concat(
-                base.GetSymbolReferences(),
+                Implements,
+                GetTypeFunction.GetSymbolReferences(),
+                Methods.GetSymbolReferences(),
+                Functions.GetSymbolReferences(),
                 Properties.GetSymbolReferences(),
                 Fields.GetSymbolReferences(),
                 Signals.GetSymbolReferences()
