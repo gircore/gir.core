0b10cadc20ebca7d4094ac74a4622eda2f5919be
badcel <1218031+badcel@users.noreply.github.com>
2021-02-18 18:38:04 +0100

    Generator: Include class struct in class

---
+++ Repository/Services/ClassStructResolverService.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Repository.Analysis;
+using Repository.Model;
+
+namespace Repository.Services
+{
+    internal class TypeReferenceResolverService 
+    {
+        public void Resolve(IEnumerable<LoadedProject> projects)
+        {
+            var symbolDictionary = new SymbolDictionary();
+
+            var loadedProjects = projects.ToList();
+            foreach (var proj in loadedProjects)
+            {
+                Log.Information($"Analysing '{proj.Name}'.");
+                FillSymbolDictionary(symbolDictionary, proj.Namespace);
+
+                Log.Information("Resolving symbol references.");
+                ResolveReferences(symbolDictionary, proj);
+            }
+        }
+
+        private void FillSymbolDictionary(SymbolDictionary symbolDictionary, Namespace @namespace)
+        {
+            AddAliases(symbolDictionary, @namespace);
+
+            symbolDictionary.AddSymbols(@namespace.Name, @namespace.Classes);
+            symbolDictionary.AddSymbols(@namespace.Name, @namespace.Interfaces);
+            symbolDictionary.AddSymbols(@namespace.Name, @namespace.Callbacks);
+            symbolDictionary.AddSymbols(@namespace.Name, @namespace.Enumerations);
+            symbolDictionary.AddSymbols(@namespace.Name, @namespace.Bitfields);
+            symbolDictionary.AddSymbols(@namespace.Name, @namespace.Records);
+            symbolDictionary.AddSymbols(@namespace.Name, @namespace.Unions);
+        }
+
+        private void ResolveReferences(SymbolDictionary symbolDictionary, LoadedProject proj)
+        {
+            var view = symbolDictionary.GetView(proj.Namespace.Name);
+
+            foreach (var reference in proj.Namespace.GetSymbolReferences())
+            {
+                var symbol = view.LookupType(reference.Name);
+
+
+                ReferenceType kind = symbol switch
+                {
+                    Type t when t.Namespace.Name == proj.Namespace.Name => ReferenceType.Internal,
+                    _ => ReferenceType.External
+                };
+
+                reference.ResolveAs(symbol, kind);
+            }
+        }
+
+        private static void AddAliases(SymbolDictionary symbolDictionary, Namespace @namespace)
+        {
+            foreach (var alias in @namespace.Aliases)
+                symbolDictionary.AddAlias(@namespace.Name, alias.NativeName, alias.ManagedName);
+        }
+    }
+}
