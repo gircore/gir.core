3b70063d9555477ae65391d9458c4e710066fe2f
badcel <1218031+badcel@users.noreply.github.com>
2021-02-26 14:00:36 +0100

    XML-Model: Do not inherit methods as the original definition does not inherit the elements either.

--- Repository/Xml/RecordInfo.cs
+++ Repository/Xml/RecordInfo.cs
@@ -1,9 +1,40 @@
+using System.Collections.Generic;
 using System.Xml.Serialization;
 
 namespace Repository.Xml
 {
-    public class RecordInfo : ClassInfo
+    public class RecordInfo
     {
+        [XmlAttribute("name")]
+        public string? Name { get; set; }
+
+        [XmlElement("doc")]
+        public DocInfo? Doc { get; set; }
+
+        [XmlAttribute("type", Namespace = "http://www.gtk.org/introspection/c/1.0")]
+        public string? Type { get; set; }
+
+        [XmlAttribute("type-name", Namespace = "http://www.gtk.org/introspection/glib/1.0")]
+        public string? TypeName { get; set; }
+
+        [XmlElement("method")]
+        public List<MethodInfo> Methods { get; set; } = default!;
+        
+        [XmlElement("constructor")]
+        public List<MethodInfo> Constructors { get; set; } = default!;
+        
+        [XmlElement("function")]
+        public List<MethodInfo> Functions { get; set; } = default!;
+
+        [XmlAttribute("get-type", Namespace = "http://www.gtk.org/introspection/glib/1.0")]
+        public string? GetTypeFunction { get; set; }
+
+        [XmlElement("property")]
+        public List<PropertyInfo> Properties { get; set; } = default!;
+        
+        [XmlElement ("field")]
+        public List<FieldInfo> Fields { get; set; } = default!;
+
         [XmlAttribute("is-gtype-struct-for", Namespace = "http://www.gtk.org/introspection/glib/1.0")]
         public string? GLibIsGTypeStructFor;
 
