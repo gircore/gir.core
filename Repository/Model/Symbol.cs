1d7171ce45a49836b77f64f5343d94f7207eb483
badcel <1218031+badcel@users.noreply.github.com>
2021-02-28 21:48:13 +0100

    Repository: Move Metadata class into separate file

--- Repository/Model/Symbol.cs
+++ Repository/Model/Symbol.cs
@@ -4,20 +4,6 @@ using Repository.Analysis;
 
 namespace Repository.Model
 {
-    public class Metadata
-    {
-        private readonly Dictionary<string, object?> _metadata = new();
-
-        public object? this[string key]
-        {
-            get => _metadata.TryGetValue(key, out var value) ? value : null;
-            set => _metadata[key] = value;
-        }
-
-        public override string ToString()
-            => string.Join(", ", _metadata.Keys);
-    }
-
     public interface  ISymbolReferenceProvider
     {
         IEnumerable<SymbolReference> GetSymbolReferences();
