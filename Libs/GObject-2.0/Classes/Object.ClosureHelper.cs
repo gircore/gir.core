19eaf406e0ad06901ed7a149af5ec03724413c50
badcel <1218031+badcel@users.noreply.github.com>
2021-03-22 18:38:16 +0100

    GObject: Refactor ClosureHelper to use Closure record

--- Libs/GObject-2.0/Classes/Object.ClosureHelper.cs
+++ Libs/GObject-2.0/Classes/Object.ClosureHelper.cs
@@ -1,67 +1,39 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
 
 namespace GObject
 {
     public partial class Object
     {
-        protected internal delegate void ActionRefValues(ref Value[] items);
+        protected internal delegate void ActionRefValues(ref Value.Native.Struct[] items);
 
         private class ClosureHelper : IDisposable
         {
-            #region Fields
-
-            // We need to store a reference to MarshalCallback to
-            // prevent the delegate from being collected by the GC
-            private readonly ClosureMarshal? _marshalCallback;
             private readonly ActionRefValues? _callback;
-            private readonly Closure.ClosureSafeHandle _safeHandle;
+            private readonly Closure _closure;
 
-            #endregion
-
-            #region Properties
-
-            public IntPtr Handle => _safeHandle.IsInvalid ? IntPtr.Zero : _safeHandle.DangerousGetHandle();
-
-            #endregion
-
-            #region Constructors
+            public IntPtr Handle => _closure.Handle;
 
             public ClosureHelper(ActionRefValues action)
             {
+                _closure = new Closure(MarshalCallback);
                 _callback = action;
-                _marshalCallback = MarshalCallback;
-
-                var handle = Closure.Native.Methods.NewSimple((uint) Marshal.SizeOf(typeof(Closure)), IntPtr.Zero);
-                _safeHandle = new Closure.ClosureSafeHandle(handle);
-
-                Closure.Native.Methods.Ref(Handle);
-                Closure.Native.Methods.Sink(Handle);
-                Closure.Native.Methods.SetMarshal(Handle, _marshalCallback);
             }
 
-            #endregion
-
-            #region Methods
-
-            private void MarshalCallback(IntPtr closure, ref Value returnValue, uint nParamValues,
-                Value[] paramValues, IntPtr invocationHint, IntPtr marshalData)
+            private void MarshalCallback(Closure.Native.Struct closure, Value.Native.Struct? returnvalue, uint nparamvalues, Value.Native.Struct[] paramvalues, IntPtr? invocationhint, IntPtr? marshaldata)
             {
                 Debug.Assert(
-                    condition: paramValues.Length == nParamValues,
+                    condition: paramvalues.Length == nparamvalues,
                     message: "Values were not marshalled correctly. Breakage may occur"
                 );
 
-                _callback?.Invoke(ref paramValues);
+                _callback?.Invoke(ref paramvalues);
             }
 
             public void Dispose()
             {
-                _safeHandle.Dispose();
+                _closure.Dispose();
             }
-
-            #endregion
         }
     }
 }
---
+++ Libs/GObject-2.0/Records/Closure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GObject
+{
+    public partial record Closure : IDisposable
+    {
+        private readonly ClosureMarshalCallHandler _closureMarshalCallHandler;
+        private readonly Native.ClosureSafeHandle _handle;
+
+        public IntPtr Handle => _handle.IsInvalid ? IntPtr.Zero : _handle.DangerousGetHandle();
+
+        internal Closure(ClosureMarshal action)
+        {
+            _closureMarshalCallHandler = new ClosureMarshalCallHandler(action);
+            _handle = Native.Methods.NewSimple((uint) Marshal.SizeOf(typeof(Closure)), IntPtr.Zero);
+
+            Native.Methods.Ref(_handle);
+            Native.Methods.Sink(_handle);
+            Native.Methods.SetMarshal(_handle, _closureMarshalCallHandler.NativeCallback);
+        }
+
+        public void Dispose()
+        {
+            _closureMarshalCallHandler.Dispose();
+            _handle.Dispose();
+        }
+    }
+}
