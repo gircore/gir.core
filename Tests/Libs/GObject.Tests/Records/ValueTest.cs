2865bb395349fa43e82edb64b4f79db39eb81efe
badcel <1218031+badcel@users.noreply.github.com>
2021-03-23 19:06:57 +0100

    Add unit test

---
+++ Tests/Libs/GObject.Tests/Records/ValueTest.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GObject.Tests.Records
+{
+    [TestClass]
+    public class ValueTest
+    {
+        [DataTestMethod]
+        [DataRow(5)]
+        public void ValueFromShouldContainGivenData(object data)
+        {
+            var v = Value.From(data);
+            v.Extract().Should().Be(data);
+        }
+    }
+}
