e4fb2d51b3c4167062d58d097bf3be070c52d661
badcel <1218031+badcel@users.noreply.github.com>
2021-03-29 21:16:12 +0200

    Gio: Add simple DBusConnectionTest

---
+++ Tests/Libs/Gio-2.0.Tests/DBusConnectionTest.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gio.Tests
+{
+    [TestClass]
+    public class DBusConnectionTest
+    {
+        [TestMethod]
+        public void GetSessionBusShouldNotBeNull()
+        {
+            var obj = DBusConnection.Get(BusType.Session);
+            obj.Should().NotBeNull();
+        }
+    }
+}
