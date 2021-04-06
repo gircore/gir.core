de0d9f631d73865b0cc6b6f153cc37b86cf2f6f4
badcel <1218031+badcel@users.noreply.github.com>
2021-04-06 17:46:41 +0200

    Gtk: Add minimal window implementation

---
+++ Libs/Gtk-3.0/Classes/Window.cs
@@ -0,0 +1,18 @@
+﻿using GObject;
+
+namespace Gtk
+{
+    public partial class Window
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="Window"/>.
+        /// </summary>
+        /// <param name="title">The window title.</param>
+        public Window(string title) 
+            : this(ConstructParameter.With(Properties.Title, title))
+        {
+        }
+
+        public void ShowAll() => Native.Widget.Instance.Methods.ShowAll(Handle);
+    }
+}
