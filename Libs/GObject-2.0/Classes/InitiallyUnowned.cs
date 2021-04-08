d6381d44227cf70831252097f97ec584878b91fc
badcel <1218031+badcel@users.noreply.github.com>
2021-04-08 18:08:38 +0200

    Add TODO

--- Libs/GObject-2.0/Classes/InitiallyUnowned.cs
+++ Libs/GObject-2.0/Classes/InitiallyUnowned.cs
@@ -4,7 +4,13 @@
     {
         protected override void Initialize()
         {
-             Native.Object.Instance.Methods.RefSink(Handle);
+            // TODO Verify if this call is needed. It should not be needed as the object class
+            // always requires the information if the ref is owned by us or not.
+            // In case of "g_object_new_with_properties" it is checked if the created
+            // ref is floating. If so it is assumed that the reference is not owned by us
+            // which will result in a "ref_sink".
+            //
+            //Native.Object.Instance.Methods.RefSink(Handle);
         }
     }
 }
