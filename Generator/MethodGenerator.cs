64d453535a8ee62c0b3f9545c5ff4f4263c707be
Matthew Jakeman <mjak923@aucklanduni.ac.nz>
2021-04-12 05:02:20 +1200

    Add delegate lifecycle management

--- Generator/MethodGenerator.cs
+++ Generator/MethodGenerator.cs
@@ -1,4 +1,6 @@
-﻿using Repository.Model;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Repository.Model;
 
 namespace Generator
 {
@@ -8,20 +10,61 @@ namespace Generator
         private readonly Namespace _currentNamespace;
         private BlockStack _stack = new ();
 
+        private IEnumerable<Parameter> nativeParams;
+        private IEnumerable<SingleParameter> managedParams;
+        private IEnumerable<SingleParameter> nullParams;
+        private IEnumerable<SingleParameter> delegateParams;
+        private IEnumerable<SingleParameter> marshalParams;
+
         public MethodGenerator(Method method, Namespace currentNamespace)
         {
             _method = method;
             _currentNamespace = currentNamespace;
+
+            nativeParams = method.ParameterList.GetParameters();
+            managedParams = method.ParameterList.GetManagedParameters();
+            nullParams = method.ParameterList.SingleParameters.Except(managedParams);
+            delegateParams = managedParams.Where(arg => arg.SymbolReference.GetSymbol().GetType() == typeof(Callback));
+            marshalParams = managedParams.Except(delegateParams);
         }
         
         public string Generate()
         {
             AddMethodSignature();
+            AddDelegateLifecycleManagement();
             AddParameterConversions();
             AddMethodCall();
             AddReturnValue();
             
             return _stack.Build();
+        }
+
+        private void AddDelegateLifecycleManagement()
+        {
+            // For each delegate-type parameter, we need to create a delegate
+            // handling object. This ensures that the delegate and associated
+            // resources are kept valid until the delegate is no longer needed
+            // by the C-library.
+            
+            foreach (var dlgParam in delegateParams)
+            {
+                Symbol symbol = dlgParam.SymbolReference.GetSymbol();
+                var managedName = symbol.Metadata["ManagedName"].ToString();
+                
+                var handlerType = dlgParam.CallbackScope switch
+                {
+                    Scope.Call => $"{symbol.Namespace.Name}.{managedName}CallHandler",
+                    Scope.Async => $"{symbol.Namespace.Name}.{managedName}AsyncHandler",
+                    Scope.Notified => $"{symbol.Namespace.Name}.{managedName}NotifiedHandler"
+                };
+
+                var alloc = $"var {dlgParam.SymbolName}Handler = new {handlerType}({dlgParam.SymbolName});";
+
+                _stack.Nest(new Block()
+                {
+                    Start = alloc
+                });
+            }
         }
 
         private void AddMethodSignature()
@@ -30,8 +73,8 @@ namespace Generator
             var parameterList = _method.ParameterList.WriteManaged(_currentNamespace);
             _stack.Nest(new ()
             {
-                Start = $"/*public {returnValue} {_method.SymbolName}({parameterList})\r\n{{",
-                End = "}}*/"
+                Start = $"public {returnValue} {_method.SymbolName}({parameterList})\r\n{{",
+                End = "}"
             });
         }
 
