4c346088c48e9a08db2de967b85589b28726d582
Matthew Jakeman <mjak923@aucklanduni.ac.nz>
2021-04-12 22:31:41 +1200

    Add Pixbuf sample to test generated methods

---
+++ Samples/GdkPixbuf/TestLoading/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using GdkPixbuf;
+
+Console.WriteLine("Testing Pixbuf");
+
+try
+{
+    var pixbuf = Pixbuf.NewFromFile("test.bmp");
+
+    Console.WriteLine("Loaded Pixbuf at address " + pixbuf.Handle);
+    Console.WriteLine("Width: " + pixbuf.GetWidth());
+    Console.WriteLine("Height: " + pixbuf.GetHeight());
+    Console.WriteLine("Has Alpha: " + pixbuf.GetHasAlpha());
+    Console.WriteLine("Channels: " + pixbuf.GetNChannels());
+
+    Console.WriteLine("Done!");
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Test Failed: {e.Message}");
+}
