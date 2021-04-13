474b929adc763de031d2ab21edc01fc011fbf589
badcel <1218031+badcel@users.noreply.github.com>
2021-04-13 18:17:29 +0200

    Tests: Test more primitive types

--- Tests/Libs/Gtk-3.0.Tests/PropertyTests.cs
+++ Tests/Libs/Gtk-3.0.Tests/PropertyTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Gtk.Tests
@@ -25,6 +26,39 @@ namespace Gtk.Tests
             window.Title = str;
 
             window.Title.Should().Be(str);
+        }
+
+        [DataTestMethod]
+        [DataRow(500)]
+        public void TestIntegerProperty(int value)
+        {
+            var window = new Window("TestWindow");
+            window.DefaultWidth = value;
+
+            window.DefaultWidth.Should().Be(value);
+        }
+
+        [DataTestMethod]
+        [DataRow(7u)]
+        public void TestUnsignedIntegerProperty(uint value)
+        {
+            var window = new Window("TestWindow");
+            window.BorderWidth = value;
+
+            window.BorderWidth.Should().Be(value);
+        }
+
+        [DataTestMethod]
+        [DataRow(0.5)]
+        [DataRow(0.1)]
+        [DataRow(0.9)]
+        public void TestDoubleProperty(double value)
+        {
+            var window = new Window("TestWindow");
+            window.Opacity = value;
+
+            //TODO: It lookls like double values are very unprecise?
+            Math.Round(window.Opacity, 2).Should().Be(value);
         }
     }
 }
