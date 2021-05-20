8fd61bac0c191a1122e69e35caf1ebc9b753b94e
Matt Jakeman <12368711+mjakeman@users.noreply.github.com>
2021-05-20 20:54:59 +1200

    Update DelegateGenerator.cs
    Use `is` instead of `typeof`

--- Generator/DelegateGenerator.cs
+++ Generator/DelegateGenerator.cs
@@ -97,25 +97,25 @@ namespace Generator
 
             // No union parameters
             if (parameterList.SingleParameters.Any(param =>
-                param.TypeReference.GetResolvedType().GetType() == typeof(Union)))
+                param.TypeReference.GetResolvedType().GetType() is Union))
                 return false;
 
             // No GObject array parameters
             if (parameterList.SingleParameters.Any(param =>
-                param.TypeReference.GetResolvedType().GetType() == typeof(Class) &&
+                param.TypeReference.GetResolvedType().GetType() is Class &&
                 param.TypeInformation.Array != null))
                 return false;
 
             // No delegate return values
-            if (returnValue.TypeReference.GetResolvedType().GetType() == typeof(Callback))
+            if (returnValue.TypeReference.GetResolvedType().GetType() is Callback)
                 return false;
             
             // No union return values
-            if (returnValue.TypeReference.GetResolvedType().GetType() == typeof(Union))
+            if (returnValue.TypeReference.GetResolvedType().GetType() is Union)
                 return false;
             
             // No GObject array return values
-            if (returnValue.TypeReference.GetResolvedType().GetType() == typeof(Class) &&
+            if (returnValue.TypeReference.GetResolvedType().GetType() is Class &&
                 returnValue.TypeInformation.Array != null)
                 return false;
 
