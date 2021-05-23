333c3d031fceb665c8ee3b42295dd1485d6cffa5
badcel <1218031+badcel@users.noreply.github.com>
2021-05-23 10:05:04 +0200

    Generator: Differentiate between pointed / unpointed records
    Unpointed records (value based)
    * For non array records in the gir their original record structure must be used.
    * For arrays of records in the gir their original record structure must be used inside an array.
    
    Pointed records (pointer based)
    * For non array records in the gir a pointer must be used. This could either be an SafeHandle (regular case) or an IntPtr (e.g. in case of free methods).
    * For arrays of records always IntPtr[] must be used as arrays of SafeHandles are not supported by the runtime.
    
    The method and delegate generators currently do not differentiate between pointed and unpointed records. To avoid compilation errors those cases are currently passing default values to methods and marked with a TODO flag.

--- Generator/Extensions/AnyTypeExtension.cs
+++ Generator/Extensions/AnyTypeExtension.cs
@@ -35,22 +35,13 @@ namespace Generator
                 // Arrays of byte can be marshalled automatically, no IntPtr needed
                 { TypeInformation: { Array: { } }, TypeReference: { ResolvedType: { } s } } when s.SymbolName == "byte" => "byte[]",
 
-                // Arrays of Opaque Structs, GObjects, and GInterfaces cannot be marshalled natively
-                // Instead marshal them as variable width pointer arrays (LPArray)
-                { TypeInformation: { Array: { } }, TypeReference: { ResolvedType: Record } } => "IntPtr[]",    // SafeHandles
-                { TypeInformation: { IsPointer: true, Array: { } }, TypeReference: { ResolvedType: Class } } => "IntPtr[]",     // GObjects
-                { TypeInformation: { IsPointer: true, Array: { } }, TypeReference: { ResolvedType: Interface } } => "IntPtr[]", // GInterfaces
-
-                // For non-pointer records, we want to embed the entire struct inside the parent struct
-                Field { TypeInformation: { IsPointer: false }, TypeReference: { ResolvedType: Record { } r } }
-                    => AddNamespace(currentNamespace, r.Repository.Namespace, r.GetMetadataString("StructRefName"), Target.Native),
-
-                // Use original symbol name for records (remapped to SafeHandles)
-                { TypeReference: { ResolvedType: Record r } } when useSafeHandle
-                    => AddNamespace(currentNamespace, r.Repository.Namespace, r.GetMetadataString("SafeHandleRefName"), Target.Native),
-
-                // If safe handle mode is "off", use an IntPtr instead
-                { TypeReference: { ResolvedType: Record r } } when !useSafeHandle => "IntPtr",
+                //References to records which are not using a pointer
+                {TypeReference: { ResolvedType: Record r}, TypeInformation: { IsPointer: false, Array: null}} => GetStructName(r, currentNamespace),
+                {TypeReference: { ResolvedType: Record r}, TypeInformation: { IsPointer: false, Array: { }}} => GetStructName(r, currentNamespace) + "[]",
+                
+                //References to records which are using a pointer
+                {TypeReference: { ResolvedType: Record r}, TypeInformation: { IsPointer: true, Array: null}} => GetSafeHandleName(r, currentNamespace, useSafeHandle),
+                {TypeReference: { ResolvedType: Record r}, TypeInformation: { IsPointer: true, Array: { }}} => "IntPtr[]", //Array of SafeHandle not supported by runtime
 
                 // Primitives - Marshal directly
                 { TypeInformation: { Array: { } }, TypeReference: { ResolvedType: PrimitiveValueType s } } => s.Write(Target.Native, currentNamespace) + "[]",
@@ -67,6 +58,19 @@ namespace Generator
                 { TypeInformation: { Array: { } } } => anyType.TypeReference.ResolvedType.Write(Target.Native, currentNamespace) + "[]",
                 _ => anyType.TypeReference.ResolvedType.Write(Target.Native, currentNamespace)
             };
+        }
+
+        private static string GetStructName(Record r, Namespace currentNamespace)
+        {
+            return AddNamespace(currentNamespace, r.Repository.Namespace, r.GetMetadataString("StructRefName"), Target.Native);
+        }
+        
+        private static string GetSafeHandleName(Record r, Namespace currentNamespace, bool useSafeHandle)
+        {
+            if (useSafeHandle)
+                return AddNamespace(currentNamespace, r.Repository.Namespace, r.GetMetadataString("SafeHandleRefName"), Target.Native);
+            
+            return "IntPtr";
         }
 
         private static string WriteManagedType(AnyType anyType, Namespace currentNamespace, bool useSafeHandle = true)
