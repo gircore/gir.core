c951ec74c09849729ce574b3ccc31b3b6c248f82
badcel <1218031+badcel@users.noreply.github.com>
2021-08-29 08:30:18 +0200

    Add Generator3 project

---
+++ src/Generation/Generator3/Generator3.cs
@@ -0,0 +1,7 @@
+﻿namespace Generator
+{
+    public class Generator3
+    {
+        
+    }
+}
---
+++ src/Tests/Generation/Generator.Tests/GeneratorTest.cs
@@ -0,0 +1,10 @@
+﻿using GirLoader.Output;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Generator.Tests
+{
+    [TestClass, TestCategory("UnitTest")]
+    public class GeneratorTest
+    {
+    }
+}
