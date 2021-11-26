b1cf967e2954875c8da6246c9fde9db4e67c799b
badcel <1218031+badcel@users.noreply.github.com>
2021-11-26 18:33:30 +0100

    GdkPixbuf: Fix compilation errors

--- src/Libs/GdkPixbuf-2.0/Classes/PixbufLoader.cs
+++ src/Libs/GdkPixbuf-2.0/Classes/PixbufLoader.cs
@@ -7,19 +7,19 @@ namespace GdkPixbuf
     {
         public static Pixbuf FromBytes(byte[] data)
         {
-            IntPtr handle = Native.PixbufLoader.Instance.Methods.New();
+            IntPtr handle = Internal.PixbufLoader.Instance.Methods.New();
 
             try
             {
                 using var bytes = Bytes.From(data);
 
-                Native.PixbufLoader.Instance.Methods.WriteBytes(handle, bytes.Handle, out var error);
+                Internal.PixbufLoader.Instance.Methods.WriteBytes(handle, bytes.Handle, out var error);
                 Error.ThrowOnError(error);
 
-                Native.PixbufLoader.Instance.Methods.Close(handle, out error);
+                Internal.PixbufLoader.Instance.Methods.Close(handle, out error);
                 Error.ThrowOnError(error);
 
-                return new Pixbuf(Native.PixbufLoader.Instance.Methods.GetPixbuf(handle), false);
+                return new Pixbuf(Internal.PixbufLoader.Instance.Methods.GetPixbuf(handle), false);
             }
             finally
             {
