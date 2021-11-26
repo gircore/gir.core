d16296ed95a0188cd187e9c5a44a1e5494d7a22e
badcel <1218031+badcel@users.noreply.github.com>
2021-11-26 21:53:09 +0100

    Interface generation: Add basic framework implementation

---
+++ src/Generation/Generator3/Generation/Interface/PublicFramework/PublicFrameworkModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Generator3.Generation.Interface
+{
+    public class PublicFrameworkModel
+    {
+        private readonly GirModel.Interface _interface;
+
+        public string Name => _interface.Name;
+        public string NamespaceName => _interface.Namespace.Name;
+
+        public PublicFrameworkModel(GirModel.Interface @interface)
+        {
+            _interface = @interface;
+        }
+    }
+}
