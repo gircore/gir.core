194fa57a02878743852f4a61172311621c4a0dc3
Matthew Jakeman <mjakeman26@outlook.co.nz>
2021-11-27 13:30:45 +1300

    convert: Start implementing parameter conversion

--- src/Generation/Generator3/Model/Convert.cs
+++ src/Generation/Generator3/Model/Convert.cs
@@ -1,10 +1,21 @@
-﻿namespace Generator3.Model
+﻿using System.Text;
+
+namespace Generator3.Model
 {
     public static class Convert
     {
         public static string GetConversion(Internal.Parameter from, Public.Parameter to)
         {
-            return $"// from: '{from.NullableTypeName} ({from.GetType()})' to: '{to.NullableTypeName} ({to.GetType()})'";
+            var comment = $"// from: '{from.NullableTypeName} ({from.GetType()})' to: '{to.NullableTypeName} ({to.GetType()})'";
+            
+            var builder = new StringBuilder();
+            builder.AppendLine(comment);
+            
+            if (from.GetType() == typeof(Internal.StandardParameter) &&
+                to.GetType() == typeof(Public.StandardParameter))
+                return builder.AppendLine($"{to.NullableTypeName} {to.Name}Conv = {from.Name};").ToString();
+
+            return builder.ToString();
         }
     }
 }
