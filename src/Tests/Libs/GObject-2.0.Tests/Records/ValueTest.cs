896cad1e67a9380c1084dd1f45e02720acccd9d1
badcel <1218031+badcel@users.noreply.github.com>
2021-12-05 00:00:41 +0100

    GObject value: Add more unit tests

--- src/Tests/Libs/GObject-2.0.Tests/Records/ValueTest.cs
+++ src/Tests/Libs/GObject-2.0.Tests/Records/ValueTest.cs
@@ -11,24 +11,49 @@ namespace GObject.Tests
         [DataRow(5)]
         [DataRow(true)]
         [DataRow("TestString")]
+        [DataRow(7u)]
+        [DataRow(2.0)]
+        [DataRow(2.0f)]
         public void ValueFromDataShouldContainGivenData(object data)
         {
             var v = Value.From(data);
             v.Extract().Should().Be(data);
         }
 
-        [TestMethod]
-        public void ValueReturnsExpectedType()
+        [DataTestMethod]
+        [DataRow("Hello", Internal.BasicType.String)]
+        [DataRow(true, Internal.BasicType.Boolean)]
+        [DataRow(1.5, Internal.BasicType.Double)]
+        [DataRow(1.5f, Internal.BasicType.Float)]
+        [DataRow(-7, Internal.BasicType.Int)]
+        [DataRow(7u, Internal.BasicType.UInt)]
+        [DataRow(77L, Internal.BasicType.Long)]
+        public void ValueContainsExpectedBasicType(object data, Internal.BasicType basicType)
         {
-            // Check that we have a value
-            var v = Value.From("Hello");
-            Internal.Functions.TypeCheckValue(v.Handle).Should().Be(true);
-
-            // Check we can marshal as a struct
+            var v = Value.From(data);
             Internal.Value.Struct str = Marshal.PtrToStructure<Internal.Value.Struct>(v.Handle.DangerousGetHandle());
+            str.GType.Should().Be((nuint) basicType);
+            Internal.Functions.TypeCheckValue(v.Handle).Should().Be(true);
             Internal.Functions.TypeCheckValueHolds(v.Handle, str.GType).Should().BeTrue();
-            Internal.Functions.TypeCheckValueHolds(v.Handle, (nuint) Internal.BasicType.String).Should().BeTrue();
-            str.GType.Should().Be((nuint) Internal.BasicType.String);
+            Internal.Functions.TypeCheckValueHolds(v.Handle, (nuint) basicType).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void DisposeShouldFreeUnmanagedMemory()
+        {
+            var value = 1;
+            var v = Value.From(value);
+            var ptr = v.Handle.DangerousGetHandle();
+
+            var d1 = Marshal.PtrToStructure<Internal.Value.Struct>(ptr);
+            d1.Data[0].VInt.Should().Be(value);
+            
+            v.Dispose();
+            
+            //Try to read the data again despite the fact that the value is already disposed
+            //If the value changed we can assume that the memory got freed.
+            var d2 = Marshal.PtrToStructure<Internal.Value.Struct>(ptr);
+            d2.Data[0].VInt.Should().NotBe(value);
         }
     }
 }
