b443779a06f292e72efe625a4595297c3c737c8b
badcel <1218031+badcel@users.noreply.github.com>
2021-12-09 19:27:04 +0100

    Fix InstanceParameters not using their converted name

--- src/Generation/Generator3/Model/Public/InstanceParameter/InstanceParameter.cs
+++ src/Generation/Generator3/Model/Public/InstanceParameter/InstanceParameter.cs
@@ -1,11 +1,13 @@
-﻿namespace Generator3.Model.Public
+﻿using Generator3.Renderer.Converter;
+
+namespace Generator3.Model.Public
 {
     public abstract class InstanceParameter
     {
         protected readonly GirModel.InstanceParameter Model;
         public abstract string NullableTypeName { get; }
 
-        public string Name => Model.Name;
+        public string Name => Model.GetPublicName();
         
         protected InstanceParameter(GirModel.InstanceParameter model)
         {
