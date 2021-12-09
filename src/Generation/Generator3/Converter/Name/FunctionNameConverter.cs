08f2c7696180b878fc2f1691b859d438671c4b53
badcel <1218031+badcel@users.noreply.github.com>
2021-12-09 20:32:12 +0100

    Generator defines function names and not GirLoader

---
+++ src/Generation/Generator3/Converter/Name/FunctionNameConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace Generator3.Renderer.Converter
+{
+    public static class FunctionNameConverter
+    {
+        public static string GetInternalName(this GirModel.Function function)
+        {
+            return function.Name.ToPascalCase().EscapeIdentifier();
+        }
+
+        public static string GetPublicName(this GirModel.Function function)
+        {
+            return function.Name.ToPascalCase().EscapeIdentifier();
+        }
+    }
+}
