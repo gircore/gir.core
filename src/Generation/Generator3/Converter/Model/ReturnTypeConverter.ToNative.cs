7e8e5c42156229045f203783df85e44dca456fed
badcel <1218031+badcel@users.noreply.github.com>
2021-12-18 11:02:32 +0100

    Split code into partial class

---
+++ src/Generation/Generator3/Converter/Model/ReturnTypeConverter.ToNative.cs
@@ -0,0 +1,28 @@
+﻿using ReturnTypeFactory = Generator3.Model.Public.ReturnTypeFactory;
+using GirModel;
+
+namespace Generator3.Converter
+{
+    public static partial class ReturnTypeConverter
+    {
+        public static string ToNative(this ReturnType from, string fromVariableName)
+        {
+            if (from.AnyType.Is<Pointer>())
+                return fromVariableName;
+
+            if (from.AnyType.Is<Bitfield>())
+                return fromVariableName;
+
+            if (from.AnyType.Is<Enumeration>())
+                return fromVariableName;
+
+            if (from.AnyType.Is<PrimitiveValueType>())
+                return fromVariableName;
+            
+            if(from.AnyType.Is<Record>())
+                return fromVariableName + ".Handle";
+            
+            throw new System.NotImplementedException($"Can't convert from return type {from} to native");
+        }
+    }
+}
