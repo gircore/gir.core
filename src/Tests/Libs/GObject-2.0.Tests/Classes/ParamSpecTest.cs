4bf47a17de4c588c387b515d015875a17f183c63
badcel <1218031+badcel@users.noreply.github.com>
2021-12-19 22:23:39 +0100

    Add bool param spec implementation with test

---
+++ src/Tests/Libs/GObject-2.0.Tests/Classes/ParamSpecTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GObject.Tests.Classes
+{
+    [TestClass, TestCategory("IntegrationTest")]
+    public class ParamSpecTest
+    {
+        [TestMethod]
+        public void CanCreateBooleanParamSpec()
+        {
+            var pspec = new ParamSpecBoolean(
+                name: "test", 
+                nick: "test", 
+                blurb: "test", 
+                defaultValue: false,
+                flags: ParamFlags.Writable
+            );
+            pspec.Handle.Should().NotBe(default);
+        }
+    }
+}
