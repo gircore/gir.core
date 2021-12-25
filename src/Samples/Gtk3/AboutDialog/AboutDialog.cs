25041a472acbee0067ac4e9911619d7cb6ba7387
badcel <1218031+badcel@users.noreply.github.com>
2021-12-25 11:10:24 +0100

    Simplify AboutDialog sample

--- src/Samples/Gtk3/AboutDialog/AboutDialog.cs
+++ src/Samples/Gtk3/AboutDialog/AboutDialog.cs
@@ -7,36 +7,18 @@ namespace AboutDialog
 {
     public class SampleDialog : Gtk.AboutDialog
     {
-        // TODO: Turn this into a subclass when we support that again
-        public static SampleDialog CreateDialog(string sampleName)
-        {
-            /*var dialog = new Gtk.AboutDialog();
-            dialog.Authors = new[] {"Gir.Core Developers", "badcel", "mjakeman"};
-            dialog.Comments = "Gir.Core is a C# wrapper for GObject based libraries providing a C# friendly API surface.";
-            dialog.Copyright = "© Gir.Core Developers 2021-present";
-            dialog.License = "MIT License";
-            dialog.Logo = LoadFromResource("AboutDialog.logo.svg");
-            dialog.Version = "0.1.0";
-            dialog.Website = "https://gircore.github.io/";
-            dialog.LicenseType = Gtk.License.MitX11;
-            dialog.ProgramName = $"{sampleName} - GirCore";*/
-
-            return new SampleDialog(sampleName);
-        }
-
         public SampleDialog(string sampleName)
         {
-            this.Authors = new[] { "Gir.Core Developers", "badcel", "mjakeman" };
-            this.Comments = "Gir.Core is a C# wrapper for GObject based libraries providing a C# friendly API surface.";
-            this.Copyright = "© Gir.Core Developers 2021-present";
-            this.License = "MIT License";
-            this.Logo = LoadFromResource("AboutDialog.logo.svg");
-            this.Version = "0.1.0";
-            this.Website = "https://gircore.github.io/";
-            this.LicenseType = Gtk.License.MitX11;
-            this.ProgramName = $"{sampleName} - GirCore";
+            Authors = new[] { "Gir.Core Developers", "badcel", "mjakeman" };
+            Comments = "Gir.Core is a C# wrapper for GObject based libraries providing a C# friendly API surface.";
+            Copyright = "© Gir.Core Developers 2021-present";
+            License = "MIT License";
+            Logo = LoadFromResource("AboutDialog.logo.svg");
+            Version = "0.1.0";
+            Website = "https://gircore.github.io/";
+            LicenseType = Gtk.License.MitX11;
+            ProgramName = $"{sampleName} - GirCore";
         }
-
 
         private static Pixbuf LoadFromResource(string resourceName)
         {
