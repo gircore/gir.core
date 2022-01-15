d06194f7f8be941c2885bb9c93387c4cfaa8c3e7
badcel <1218031+badcel@users.noreply.github.com>
2022-01-15 12:37:43 +0100

    Internal record methods: Do not generate if no methods are present

--- src/Generation/Generator3/Generation/Record/InternalMethods/InternalMethodsGenerator.cs
+++ src/Generation/Generator3/Generation/Record/InternalMethods/InternalMethodsGenerator.cs
@@ -1,4 +1,5 @@
-﻿using Generator3.Converter;
+﻿using System.Linq;
+using Generator3.Converter;
 
 namespace Generator3.Generation.Record
 {
@@ -17,6 +18,12 @@ namespace Generator3.Generation.Record
         {
             try
             {
+                if (!record.Constructors.Any()
+                    && !record.Methods.Any()
+                    && !record.Functions.Any()
+                    && record.TypeFunction is null)
+                    return;
+
                 var model = new InternalMethodsModel(record);
                 var source = _template.Render(model);
                 var codeUnit = new CodeUnit(record.Namespace.GetCanonicalName(), $"{record.Name}.Methods", source);
