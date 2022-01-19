a2cd5b093f09ee30a207d69729340e91ead69bfe
badcel <1218031+badcel@users.noreply.github.com>
2022-01-19 16:12:45 +0100

    Remove obsolete properties

--- src/Generation/Generator3/Generation/Record/PublicClass/PublicClassModel.cs
+++ src/Generation/Generator3/Generation/Record/PublicClass/PublicClassModel.cs
@@ -8,8 +8,6 @@ namespace Generator3.Generation.Record
 
         public string Name => _record.GetPublicClassName();
         public string InternalHandleName => _record.GetInternalHandleName();
-        public string InternalOwnedHandleName => _record.GetInternalOwnedHandleName();
-        public string InternalUnownedHandleName => _record.GetInternalUnownedHandleName();
         public string NamespaceName => _record.Namespace.Name;
 
         public PublicClassModel(GirModel.Record record)
