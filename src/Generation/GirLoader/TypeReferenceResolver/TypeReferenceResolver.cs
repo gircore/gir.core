1ad4419ce340cf7d681ded9db95cde0701240bd8
badcel <1218031+badcel@users.noreply.github.com>
2022-02-16 17:58:55 +0100

    Improve type reference resolving The new implementation changes the priorities for resolving references.
    New order:
    1. Own types
    2. Own aliases
    3. Foreign types (only transitive dependencies in correct order)
    4. Foreign aliases (only transitive dependencies in correct order)
    5. Global types
    
    Old order:
    1. Own aliases
    2. Foreign aliases (unordered transitive dependencies)
    3. Own types
    4. Foreign types (all known repositories even not depending ones)
    5. Global types

--- src/Generation/GirLoader/TypeReferenceResolver/TypeReferenceResolver.cs
+++ src/Generation/GirLoader/TypeReferenceResolver/TypeReferenceResolver.cs
@@ -2,66 +2,93 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using GirLoader.Helper;
 
-namespace GirLoader
+namespace GirLoader;
+
+internal partial class TypeReferenceResolver
 {
-    internal partial class TypeReferenceResolver
-    {
-        private readonly Dictionary<Output.Repository, TypeCache> _data = new();
-        private readonly TypeCache _globalTypes = new GlobalTypeCache();
+    private readonly Dictionary<Output.Repository, TypeCache> _typeCaches = new();
+    private readonly TypeCache _globalTypes = new GlobalTypeCache();
 
-        public bool Resolve(Output.TypeReference typeReference, Output.Repository repository, [MaybeNullWhen(false)] out Output.Type type)
-        {
-            if (TryResolveAlias(typeReference, repository, out type))
+    public bool Resolve(Output.TypeReference typeReference, Output.Repository repository, [MaybeNullWhen(false)] out Output.Type type)
+    {
+        if (TryResolveFromType(typeReference, repository, out type))
+            return true;
+
+        if (TryResolveFromAlias(typeReference, repository, out type))
+            return true;
+
+        if (TryResolveFromForeignType(typeReference, repository, out type))
+            return true;
+
+        if (TryResolveFromForeignAlias(typeReference, repository, out type))
+            return true;
+
+        if (_globalTypes.TryResolve(typeReference, out type))
+            return true;
+
+        return false;
+    }
+
+    private bool TryResolveFromType(Output.TypeReference typeReference, Output.Repository repository, [MaybeNullWhen(false)] out Output.Type type)
+    {
+        if (_typeCaches.TryGetValue(repository, out var typeCache))
+            if (typeCache.TryResolve(typeReference, out type))
                 return true;
 
-            //Prefer current namespace to resolve types before trying other ones
-            if (_data.TryGetValue(repository, out var typeCache))
+        type = null;
+        return false;
+    }
+
+    private bool TryResolveFromAlias(Output.TypeReference typeReference, Output.Repository repository, [MaybeNullWhen(false)] out Output.Type type)
+    {
+        var directResult = repository.Namespace.Aliases.FirstOrDefault(x => x.Matches(typeReference));
+
+        if (directResult is { })
+        {
+            type = directResult.TypeReference.GetResolvedType();
+            return true;
+        }
+
+        type = null;
+        return false;
+    }
+
+    private bool TryResolveFromForeignType(Output.TypeReference typeReference, Output.Repository repository, [MaybeNullWhen(false)] out Output.Type type)
+    {
+        var orderedDependentRepositories = repository.Dependencies.OrderByDependencies();
+        orderedDependentRepositories.Reverse(); //Reverse order to iterate from most specific to most generic dependency
+
+        foreach (var dependentRepository in orderedDependentRepositories)
+            if (_typeCaches.TryGetValue(dependentRepository, out var typeCache))
                 if (typeCache.TryResolve(typeReference, out type))
                     return true;
 
-            foreach (var cache in _data.Values)
-            {
-                if (cache.TryResolve(typeReference, out type))
-                    return true;
-            }
+        type = null;
+        return false;
+    }
 
-            if (_globalTypes.TryResolve(typeReference, out type))
+    private bool TryResolveFromForeignAlias(Output.TypeReference typeReference, Output.Repository repository, [MaybeNullWhen(false)] out Output.Type type)
+    {
+        var orderedDependentRepositories = repository.Dependencies.OrderByDependencies();
+        orderedDependentRepositories.Reverse(); //Reverse order to iterate from most specific to most generic dependency
+
+        foreach (var r in orderedDependentRepositories)
+        {
+            if (TryResolveFromAlias(typeReference, r, out type))
                 return true;
-
-            return false;
         }
 
-        private bool TryResolveAlias(Output.TypeReference typeReference, Output.Repository repository, [MaybeNullWhen(false)] out Output.Type type)
-        {
-            type = RecursiveResolveAlias(repository, typeReference);
+        type = null;
+        return false;
+    }
 
-            return type is { };
-        }
+    public void AddRepository(Output.Repository repository)
+    {
+        if (_typeCaches.ContainsKey(repository))
+            throw new Exception($"The repository contains an already known repository {repository}.");
 
-        private static Output.Type? RecursiveResolveAlias(Output.Repository repository, Output.TypeReference typeReference)
-        {
-            var directResult = repository.Namespace.Aliases.FirstOrDefault(x => x.Matches(typeReference));
-
-            if (directResult is { })
-                return directResult.TypeReference.GetResolvedType();
-
-            foreach (var parent in repository.Dependencies)
-            {
-                var parentResult = RecursiveResolveAlias(parent, typeReference);
-                if (parentResult is { })
-                    return parentResult;
-            }
-
-            return null;
-        }
-
-        public void AddRepository(Output.Repository repository)
-        {
-            if (_data.ContainsKey(repository))
-                throw new Exception($"The repository contains an already known repository {repository}.");
-
-            _data.Add(repository, new RepositoryTypeCache(repository));
-        }
+        _typeCaches.Add(repository, new RepositoryTypeCache(repository));
     }
 }
