da6f4d7a3d92c0634ca835bd832a551ee4637ac4
badcel <1218031+badcel@users.noreply.github.com>
2022-03-03 20:27:13 +0100

    Convert AboutDialog sample to GTK4

---
+++ src/Samples/Gtk4/AboutDialog/Program.cs
@@ -0,0 +1,11 @@
+﻿using Gtk;
+
+var application = Application.New("org.gir.core", Gio.ApplicationFlags.FlagsNone);
+application.OnActivate += (sender, args) =>
+{
+    var dialog = new AboutDialog.SampleAboutDialog("Custom AboutDialog");
+    dialog.Application = (Application) sender;
+    dialog.Show();
+};
+
+return application.Run();
