e727160a864c1accd41aa258f53d152423b0e258
badcel <1218031+badcel@users.noreply.github.com>
2022-04-17 06:52:16 +0200

    GirTool: Split GenerateCommand class into definition and logic part

---
+++ src/GirTool/GenerateCommand.Definition.cs
@@ -0,0 +1,65 @@
+﻿using System.CommandLine;
+using System.CommandLine.Binding;
+using System.CommandLine.Invocation;
+
+namespace GirTool;
+
+public partial class GenerateCommand : Command
+{
+    public GenerateCommand() : base(
+        name: "generate",
+        description: "Generate C# bindings from gir files"
+    )
+    {
+        var inputArgument = new Argument<string[]>(
+            name: "input",
+            description: "The names of gir files which should be processed"
+        );
+
+        var outputOption = new Option<string>(
+            aliases: new[] { "-o", "--output" },
+            description: "The directory to write the generated C# files to",
+            getDefaultValue: () => "./Libs"
+        );
+
+        var searchPathOptionLinux = new Option<string>(
+            aliases: new[] { "-sl", "--search-path-linux" },
+            description: "The directory which is searched for dependent linux gir files"
+        );
+
+        var searchPathOptionMacos = new Option<string>(
+            aliases: new[] { "-sm", "--search-path-macos" },
+            description: "The directory which is searched for dependent macos gir files"
+        );
+
+        var searchPathOptionWindows = new Option<string>(
+            aliases: new[] { "-sw", "--search-path-windows" },
+            description: "The directory which is searched for dependent windows gir files"
+        );
+
+        var disableAsyncOption = new Option<bool>(
+            aliases: new[] { "-d", "--disable-async" },
+            getDefaultValue: () => false,
+            description: "Generate files synchronously - useful for debugging"
+        );
+
+        var logLevelOption = new Option<LogLevel>(
+            aliases: new[] { "-l", "--log-level" },
+            getDefaultValue: () => LogLevel.Standard,
+            description: "Set the log level"
+        );
+
+        AddArgument(inputArgument);
+        AddOption(outputOption);
+        AddOption(searchPathOptionLinux);
+        AddOption(searchPathOptionMacos);
+        AddOption(searchPathOptionWindows);
+        AddOption(disableAsyncOption);
+        AddOption(logLevelOption);
+
+        this.SetHandler<string[], string, string?, string?, string?, bool, LogLevel, InvocationContext>(
+            handle: Execute,
+            symbols: new IValueDescriptor[] { inputArgument, outputOption, searchPathOptionLinux, searchPathOptionMacos, searchPathOptionWindows, disableAsyncOption, logLevelOption }
+        );
+    }
+}
--- src/GirTool/GenerateCommand.cs
+++ src/GirTool/GenerateCommand.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.CommandLine;
-using System.CommandLine.Binding;
 using System.CommandLine.Invocation;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Generator3;
 using Generator3.Converter;
-using Generator3.Fixer.Public;
 using Generator3.Publication;
 using GirLoader;
 using GirLoader.PlatformSupport;
@@ -16,65 +13,8 @@ using Repository = GirLoader.Output.Repository;
 
 namespace GirTool;
 
-public class GenerateCommand : Command
+public partial class GenerateCommand : Command
 {
-    public GenerateCommand() : base(
-        name: "generate",
-        description: "Generate C# bindings from gir files"
-    )
-    {
-        var inputArgument = new Argument<string[]>(
-            name: "input",
-            description: "The names of gir files which should be processed"
-        );
-
-        var outputOption = new Option<string>(
-            aliases: new[] { "-o", "--output" },
-            description: "The directory to write the generated C# files to",
-            getDefaultValue: () => "./Libs"
-        );
-
-        var searchPathOptionLinux = new Option<string>(
-            aliases: new[] { "-sl", "--search-path-linux" },
-            description: "The directory which is searched for dependent linux gir files"
-        );
-
-        var searchPathOptionMacos = new Option<string>(
-            aliases: new[] { "-sm", "--search-path-macos" },
-            description: "The directory which is searched for dependent macos gir files"
-        );
-
-        var searchPathOptionWindows = new Option<string>(
-            aliases: new[] { "-sw", "--search-path-windows" },
-            description: "The directory which is searched for dependent windows gir files"
-        );
-
-        var disableAsyncOption = new Option<bool>(
-            aliases: new[] { "-d", "--disable-async" },
-            getDefaultValue: () => false,
-            description: "Generate files synchronously - useful for debugging"
-        );
-
-        var logLevelOption = new Option<LogLevel>(
-            aliases: new[] { "-l", "--log-level" },
-            getDefaultValue: () => LogLevel.Standard,
-            description: "Set the log level"
-        );
-
-        AddArgument(inputArgument);
-        AddOption(outputOption);
-        AddOption(searchPathOptionLinux);
-        AddOption(searchPathOptionMacos);
-        AddOption(searchPathOptionWindows);
-        AddOption(disableAsyncOption);
-        AddOption(logLevelOption);
-
-        this.SetHandler<string[], string, string?, string?, string?, bool, LogLevel, InvocationContext>(
-            handle: Execute,
-            symbols: new IValueDescriptor[] { inputArgument, outputOption, searchPathOptionLinux, searchPathOptionMacos, searchPathOptionWindows, disableAsyncOption, logLevelOption }
-        );
-    }
-
     private void Execute(string[] input, string output, string? searchPathLinux, string? searchPathMacos, string? searchPathWindows, bool disableAsync, LogLevel logLevel, InvocationContext invocationContext)
     {
         try
@@ -104,7 +44,7 @@ public class GenerateCommand : Command
         }
     }
 
-    private IEnumerable<PlatformHandler> GetPlatformHandler(string? searchPathLinux, string? searchPathMacos, string? searchPathWindows, bool disableAsync, string[] input, InvocationContext invocationContext)
+    private static IEnumerable<PlatformHandler> GetPlatformHandler(string? searchPathLinux, string? searchPathMacos, string? searchPathWindows, bool disableAsync, string[] input, InvocationContext invocationContext)
     {
         try
         {
@@ -151,7 +91,7 @@ public class GenerateCommand : Command
         return Enumerable.Empty<PlatformHandler>();
     }
 
-    private (List<Repository>, List<Repository>, List<Repository>) LoadRepositories(string? searchPathLinux, string? searchPathMacos, string? searchPathWindows, bool disableAsync, string[] input)
+    private static (List<Repository>, List<Repository>, List<Repository>) LoadRepositories(string? searchPathLinux, string? searchPathMacos, string? searchPathWindows, bool disableAsync, string[] input)
     {
         if (searchPathLinux is null && searchPathMacos is null && searchPathWindows is null)
             throw new Exception("Please define at least one search parth for a specific platform");
@@ -182,7 +122,7 @@ public class GenerateCommand : Command
         return (linuxRepositories!, macosRepositories!, windowsRepositories!);
     }
 
-    private List<Repository> DeserializeInput(string? searchPath, string[] input)
+    private static List<Repository> DeserializeInput(string? searchPath, string[] input)
     {
         if (searchPath is null)
             return new List<Repository>();
