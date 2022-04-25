5443170e771e4925034a94047db9f0e88b30d2fc
Mikkel Kruse Johnsen <mikkel@xmedicus.com>
2022-04-25 14:35:29 +0200

    Add support for Gtk.Builder from UI files.

---
+++ src/Samples/Gtk4/Builder/Program.cs
@@ -0,0 +1,12 @@
+﻿using Gtk;
+
+var application = Application.New("org.gir.core", Gio.ApplicationFlags.FlagsNone);
+application.OnActivate += (sender, args) =>
+{
+    var dialog = new BuilderSample.SampleTestDialog();
+    dialog.Application = (Application) sender;
+    dialog.Show();
+    dialog.OnResponse += (_, _) => application.Quit();
+};
+
+return application.Run();
