ad079b74fead0c8ddcd67c83eaf415f88aa0f1f2
Cameron White <cameronwhite91@gmail.com>
2022-05-29 13:58:39 -0400

    Add the base and toy bindings for Cairo.FontFace
    This adds the base cairo_font_face_t methods, along with the toy font face implementation from https://cairographics.org/manual/cairo-text.html. Other font face subclasses are platform-specific and are more suitable for direct use from the C API (https://cairographics.org/manual/bindings-fonts.html)

--- src/Libs/cairo-1.0/Internal/Records/FontFace.cs
+++ src/Libs/cairo-1.0/Internal/Records/FontFace.cs
@@ -7,5 +7,11 @@ namespace Cairo.Internal
     {
         [DllImport(CairoImportResolver.Library, EntryPoint = "cairo_font_face_destroy")]
         public static extern void Destroy(IntPtr handle);
+
+        [DllImport(CairoImportResolver.Library, EntryPoint = "cairo_font_face_get_type")]
+        public static extern FontType GetType(FontFaceHandle handle);
+
+        [DllImport(CairoImportResolver.Library, EntryPoint = "cairo_font_face_status")]
+        public static extern Status Status(FontFaceHandle handle);
     }
 }
