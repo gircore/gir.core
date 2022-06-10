1a01b018182ae2340bc32f7fc4195bebf758dcec
badcel <1218031+badcel@users.noreply.github.com>
2022-06-10 19:30:17 +0200

    Bump System.CommandLine to 2.0.0-beta4.22272.1

--- src/GirTool/GenerateCommand.Definition.cs
+++ src/GirTool/GenerateCommand.Definition.cs
@@ -1,5 +1,5 @@
-﻿using System.CommandLine;
-using System.CommandLine.Binding;
+﻿using System;
+using System.CommandLine;
 using System.CommandLine.Invocation;
 
 namespace GirTool;
@@ -57,9 +57,15 @@ public partial class GenerateCommand : Command
         AddOption(disableAsyncOption);
         AddOption(logLevelOption);
 
-        this.SetHandler<string[], string, string?, string?, string?, bool, LogLevel, InvocationContext>(
-            handle: Execute,
-            symbols: new IValueDescriptor[] { inputArgument, outputOption, searchPathOptionLinux, searchPathOptionMacos, searchPathOptionWindows, disableAsyncOption, logLevelOption }
-        );
+        this.SetHandler(context => Execute(
+            input: context.ParseResult.GetValueForArgument(inputArgument),
+            output: context.ParseResult.GetValueForOption(outputOption) ?? throw new Exception("Output unknown"),
+            searchPathLinux: context.ParseResult.GetValueForOption(searchPathOptionLinux),
+            searchPathMacos: context.ParseResult.GetValueForOption(searchPathOptionMacos),
+            searchPathWindows: context.ParseResult.GetValueForOption(searchPathOptionWindows),
+            disableAsync: context.ParseResult.GetValueForOption(disableAsyncOption),
+            logLevel: context.ParseResult.GetValueForOption(logLevelOption),
+            invocationContext: context
+        ));
     }
 }
