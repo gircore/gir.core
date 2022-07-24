7995cf51f502a042663c20ef7df67ee5ee73ad2e
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-07-24 11:19:00 +0300

    Changed member generation formatting to use invariant culture
    this fixes generating enums in different user locales

--- src/Generation/Generator3/Renderer/Public/Member.cs
+++ src/Generation/Generator3/Renderer/Public/Member.cs
@@ -1,8 +1,10 @@
-﻿namespace Generator3.Renderer.Public
+﻿using static System.FormattableString;
+
+namespace Generator3.Renderer.Public
 {
     public static class Member
     {
         public static string Render(this Model.Public.Member member)
-            => $"{ member.Name } = { member.Value },";
+            => Invariant($"{ member.Name } = { member.Value },");
     }
 }
