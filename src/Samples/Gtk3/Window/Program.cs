714821b81f4ba59299b14fdb85c26539ce339dad
badcel <1218031+badcel@users.noreply.github.com>
2022-09-12 17:44:40 +0200

    Remove Gtk-3.0 Window sample as there is a Gtk-4.0 sample

--- src/Samples/Gtk3/Window/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Gtk;
-
-Gtk.Module.Initialize();
-
-var mainWindow = new Window("Hello Gtk!");
-mainWindow.OnDestroy += (obj, args) => Functions.MainQuit();
-mainWindow.ShowAll();
-
-Functions.Main();
