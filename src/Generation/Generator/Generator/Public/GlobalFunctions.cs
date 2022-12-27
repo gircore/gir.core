ab25d23d27ac9adccae3f8614f3eeae17d315859
Cameron White <cameronwhite91@gmail.com>
2022-12-26 20:05:56 -0500

    Support global functions
    - Add bindings for global functions (e.g. g_set_application_name()), as static functions like `GLib.Functions.SetApplicationName()`
    
    - Remove manual binding for GObject.Functions.TypeIsA, which is now automatically generated

---
+++ src/Generation/Generator/Generator/Public/GlobalFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Generator.Model;
+
+namespace Generator.Generator.Public;
+
+internal class GlobalFunctions : Generator<IEnumerable<GirModel.Function>>
+{
+    private readonly Publisher _publisher;
+
+    public GlobalFunctions(Publisher publisher)
+    {
+        _publisher = publisher;
+    }
+
+    public void Generate(IEnumerable<GirModel.Function> functions)
+    {
+        var source = Renderer.Public.GlobalFunctions.Render(functions);
+        var codeUnit = new CodeUnit(
+            Project: Namespace.GetCanonicalName(functions.First().Namespace),
+            Name: "Functions",
+            Source: source,
+            IsInternal: false
+        );
+
+        _publisher.Publish(codeUnit);
+    }
+}
---
+++ src/Tests/Libs/GLib-2.0.Tests/FunctionTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GLib.Tests;
+
+[TestClass, TestCategory("IntegrationTest")]
+public class FunctionTest : Test
+{
+    [TestMethod]
+    public void CanSetApplicationName()
+    {
+        // Simple test of global functions.
+        GLib.Functions.GetApplicationName().Should().Be("");
+        GLib.Functions.SetApplicationName("foo");
+        GLib.Functions.GetApplicationName().Should().Be("foo");
+    }
+}
+
