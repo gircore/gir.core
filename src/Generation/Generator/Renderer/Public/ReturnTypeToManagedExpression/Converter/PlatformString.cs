7c63e82885ac061cdc04ea0349d5eb8695bc7d09
badcel <1218031+badcel@users.noreply.github.com>
2023-01-22 21:58:24 +0100

    Improve usage of StringHelper ReturnTypes respect nullable attributes for strings. If a null is returned for a non nullable string return type an exception is thrown during runtime.

--- src/Generation/Generator/Renderer/Public/ReturnTypeToManagedExpression/Converter/PlatformString.cs
+++ src/Generation/Generator/Renderer/Public/ReturnTypeToManagedExpression/Converter/PlatformString.cs
@@ -10,10 +10,14 @@ internal class PlatformString : ReturnTypeConverter
 
     public string GetString(GirModel.ReturnType returnType, string fromVariableName)
     {
+        var nullCheck = returnType.Nullable
+            ? string.Empty
+            : " ?? throw new System.Exception(\"Non nullable return type returned a null\")";
+
         //If ownership is transfered the internal return type is encoded as a string as the
         //marshaller will handle the ownership transfer automatically
         return Transfer.IsOwnedRef(returnType.Transfer)
             ? fromVariableName
-            : $"GLib.Internal.StringHelper.ToStringUtf8({fromVariableName})";
+            : $"GLib.Internal.StringHelper.ToStringUtf8({fromVariableName}){nullCheck}";
     }
 }
--- src/Tests/Libs/GLib-2.0.Tests/FunctionTest.cs
+++ src/Tests/Libs/GLib-2.0.Tests/FunctionTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GLib.Tests;
@@ -11,7 +10,7 @@ public class FunctionTest : Test
     public void CanSetApplicationName()
     {
         // Simple test of global functions.
-        GLib.Functions.GetApplicationName().Should().Be("");
+        GLib.Functions.GetApplicationName().Should().BeNull();
         GLib.Functions.SetApplicationName("foo");
         GLib.Functions.GetApplicationName().Should().Be("foo");
     }
