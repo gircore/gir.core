7489f8bee9936e6a3e23469148cd1f98ab5396cf
Cameron White <cameronwhite91@gmail.com>
2023-04-30 13:10:56 -0400

    Add an event for unhandled exceptions from delegates invoked by native code
    These unhandled exceptions would otherwise immediately terminate the program once they reach the native code boundary. This event lets applications do any logging / message dialogs etc and decide whether they want to terminate the program or attempt to continue.
    
    Bug: #845

---
+++ src/Libs/GLib-2.0/Public/UnhandledException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GLib;
+
+/// <summary>
+/// Allows handlings exceptions which can't cross the native code boundary and
+/// would terminate the application.
+/// </summary>
+public static class UnhandledException
+{
+    /// <summary>
+    /// Invoked if an exception is raised which reaches the native code boundary.
+    /// This can be used by applications to handle these exceptions gracefully
+    /// without terminating the application.
+    /// </summary>
+    public static event EventHandler<UnhandledExceptionEventArgs>? Raised;
+
+    /// <summary>
+    /// Call this method to invoke the "Raised" event. If the event has no subscribers,
+    /// the application is terminated.
+    /// </summary>
+    /// <remarks>
+    /// This method is not intended to be called by application code, and should only
+    /// be called by code which catches an exception that would otherwise terminate
+    /// the application by unwinding to the native code boundary.
+    /// </remarks>
+    public static void Raise(Exception e)
+    {
+        if (Raised is null)
+        {
+            Console.Error.WriteLine($"{nameof(GLib.UnhandledException)} - unhandled exception: {e}");
+            Environment.Exit(1);
+        }
+        else
+        {
+            Raised.Invoke(null, new UnhandledExceptionEventArgs(e, false));
+        }
+    }
+}
---
+++ src/Tests/Libs/GirTest-0.1.Tests/UnhandledExceptionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GirTest.Tests;
+
+[TestClass, TestCategory("BindingTest")]
+public class UnhandledExceptionTest : Test
+{
+    [TestMethod]
+    public void SupportsSignalHandlers()
+    {
+        var tester = ReturningSignalTester.New();
+        tester.OnReturnBool += (_, _) =>
+        {
+            throw new NotImplementedException();
+        };
+
+        bool exceptionCaught = false;
+        GLib.UnhandledException.Raised += (_, args) =>
+        {
+            exceptionCaught = args.ExceptionObject is NotImplementedException;
+        };
+
+        tester.EmitReturnBool().Should().Be(false);
+        exceptionCaught.Should().Be(true);
+    }
+}
