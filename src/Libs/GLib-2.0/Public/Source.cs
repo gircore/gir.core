7bcc49a505a4ec43f63ab1143e85d446ee062a04
badcel <1218031+badcel@users.noreply.github.com>
2023-05-04 22:27:12 +0200

    Manual GLib implementations

--- src/Libs/GLib-2.0/Public/Source.cs
+++ src/Libs/GLib-2.0/Public/Source.cs
@@ -1,6 +1,19 @@
-﻿namespace GLib;
+﻿using System;
+
+namespace GLib;
 
 public partial class Source
 {
+    public void SetCallback(SourceFunc sourceFunc)
+    {
+        var handler = new Internal.SourceFuncNotifiedHandler(sourceFunc);
+        Internal.Source.SetCallback(Handle, handler.NativeCallback, IntPtr.Zero, handler.DestroyNotify);
+    }
+
+    public void Attach(MainContext mainContext)
+    {
+        Internal.Source.Attach(Handle, mainContext.Handle);
+    }
+
     public static void Remove(uint tag) => Internal.Functions.SourceRemove(tag);
 }
