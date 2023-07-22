513c00df095db603eb958e94e62494e05b8ae67c
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-07-22 20:23:50 +0300

    Add manual bindings for Gtk.FileDialog

---
+++ src/Libs/Gtk-4.0/Public/FileDialog.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Gtk;
+
+public partial class FileDialog
+{
+    //TODO: Async methods should be generated automatically (https://github.com/gircore/gir.core/issues/893)
+    public Task<Gio.File?> OpenAsync(Window parent)
+    {
+        var tcs = new TaskCompletionSource<Gio.File?>();
+
+        void Callback(IntPtr sourceObject, IntPtr res, IntPtr userData)
+        {
+            var fileValue = Internal.FileDialog.OpenFinish(sourceObject, res, out var error);
+            GLib.Error.ThrowOnError(error);
+
+            if (fileValue == IntPtr.Zero)
+            {
+                tcs.SetResult(null);
+            }
+            else
+            {
+                var value = new Gio.FileHelper(fileValue, true);
+                tcs.SetResult(value);
+            }
+        }
+
+        Internal.FileDialog.Open(
+            self: Handle,
+            parent: parent.Handle,
+            cancellable: IntPtr.Zero,
+            callback: Callback,
+            userData: IntPtr.Zero
+        );
+
+        return tcs.Task;
+    }
+
+    //TODO: Async methods should be generated automatically (https://github.com/gircore/gir.core/issues/893)
+    public Task<Gio.File?> SaveAsync(Window parent)
+    {
+        var tcs = new TaskCompletionSource<Gio.File?>();
+
+        void Callback(IntPtr sourceObject, IntPtr res, IntPtr userData)
+        {
+            var fileValue = Internal.FileDialog.SaveFinish(sourceObject, res, out var error);
+            GLib.Error.ThrowOnError(error);
+
+            if (fileValue == IntPtr.Zero)
+            {
+                tcs.SetResult(null);
+            }
+            else
+            {
+                var value = new Gio.FileHelper(fileValue, true);
+                tcs.SetResult(value);
+            }
+        }
+
+        Internal.FileDialog.Save(
+            self: Handle,
+            parent: parent.Handle,
+            cancellable: IntPtr.Zero,
+            callback: Callback,
+            userData: IntPtr.Zero
+        );
+
+        return tcs.Task;
+    }
+
+
+    //TODO: Async methods should be generated automatically (https://github.com/gircore/gir.core/issues/893)
+    public Task<Gio.File?> SelectFolderAsync(Window parent)
+    {
+        var tcs = new TaskCompletionSource<Gio.File?>();
+
+        void Callback(IntPtr sourceObject, IntPtr res, IntPtr userData)
+        {
+            var fileValue = Internal.FileDialog.SelectFolderFinish(sourceObject, res, out var error);
+            GLib.Error.ThrowOnError(error);
+
+            if (fileValue == IntPtr.Zero)
+            {
+                tcs.SetResult(null);
+            }
+            else
+            {
+                var value = new Gio.FileHelper(fileValue, true);
+                tcs.SetResult(value);
+            }
+        }
+
+        Internal.FileDialog.SelectFolder(
+            self: Handle,
+            parent: parent.Handle,
+            cancellable: IntPtr.Zero,
+            callback: Callback,
+            userData: IntPtr.Zero
+        );
+
+        return tcs.Task;
+    }
+}
