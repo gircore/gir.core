57f79da692773723c103fde2b3f6ed3ef0a18152
badcel <1218031+badcel@users.noreply.github.com>
2023-07-23 20:02:01 +0200

    GException: - Make GException constructor public - Rename file to match class name - Remove #region tags

--- src/Libs/GLib-2.0/Public/GException.cs
+++ src/Libs/GLib-2.0/Public/GException.cs
@@ -3,31 +3,19 @@ using System.Runtime.InteropServices;
 
 namespace GLib;
 
-public sealed partial class GException : Exception, IDisposable
+public sealed class GException : Exception, IDisposable
 {
-    #region Fields
-
     private readonly Internal.ErrorHandle _errorHandle;
 
-    #endregion
-
-    #region Constructors
-
-    internal GException(Internal.ErrorHandle errorHandle)
+    public GException(Internal.ErrorHandle errorHandle)
         : base(Marshal.PtrToStructure<Internal.ErrorData>(errorHandle.DangerousGetHandle()).Message)
     {
 
         _errorHandle = errorHandle;
     }
 
-    #endregion
-
-    #region Methods
-
     public void Dispose()
     {
         _errorHandle.Dispose();
     }
-
-    #endregion
 }
