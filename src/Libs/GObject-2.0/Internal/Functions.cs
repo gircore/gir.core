61c6e32ec98c11a4e03f27d4471456ddf64832cd
badcel <1218031+badcel@users.noreply.github.com>
2023-07-28 21:50:18 +0200

    Use ImportResolver constant instead of string to define library name

--- src/Libs/GObject-2.0/Internal/Functions.cs
+++ src/Libs/GObject-2.0/Internal/Functions.cs
@@ -7,6 +7,6 @@ public partial class Functions
     // "g_strv_get_type" method is defined in GLib gir file but is
     // historically part of the GObject shared library. This is
     // why it is defined manually here.
-    [DllImport("GObject", EntryPoint = "g_strv_get_type")]
+    [DllImport(ImportResolver.Library, EntryPoint = "g_strv_get_type")]
     internal static extern nuint StrvGetType();
 }
