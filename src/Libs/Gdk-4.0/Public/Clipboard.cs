7b1dcf1baa7717e1b23ef87ad3c96f9a4c039a88
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-09-16 21:47:13 +0300

    Add manual bindings for Gdk.Clipboard.ReadTextAsync

---
+++ src/Libs/Gdk-4.0/Public/Clipboard.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Gdk;
+
+public partial class Clipboard
+{
+    //TODO: Async methods should be generated automatically (https://github.com/gircore/gir.core/issues/893)
+    public Task<string?> ReadTextAsync()
+    {
+        var tcs = new TaskCompletionSource<string?>();
+
+        var callbackHandler = new Gio.Internal.AsyncReadyCallbackAsyncHandler((sourceObject, res, data) =>
+        {
+            if (sourceObject is null)
+            {
+                tcs.SetException(new Exception("Missing source object"));
+                return;
+            }
+
+            var readValue = Internal.Clipboard.ReadTextFinish(sourceObject.Handle, res.Handle, out var error);
+
+            if (!error.IsInvalid)
+                tcs.SetException(new GLib.GException(error));
+            else
+                tcs.SetResult(readValue.ConvertToString());
+        });
+
+        Internal.Clipboard.ReadTextAsync(
+            clipboard: Handle,
+            cancellable: IntPtr.Zero,
+            callback: callbackHandler.NativeCallback,
+            userData: IntPtr.Zero
+        );
+
+        return tcs.Task;
+    }
+}
