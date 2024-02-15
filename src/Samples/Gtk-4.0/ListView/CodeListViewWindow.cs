4331c7ed95101418fe481515091b2921b0dc251f
Kashif Soofi <kashif.soofi+git@gmail.com>
2024-02-15 22:59:12 +0000

    ListView sample with code only and ListItem template

---
+++ src/Samples/Gtk-4.0/ListView/CodeListViewWindow.cs
@@ -0,0 +1,55 @@
+﻿using Gtk;
+using static Gtk.SignalListItemFactory;
+
+namespace ListViewSample;
+
+public class CodeListViewWindow : Window
+{
+    public CodeListViewWindow()
+    {
+        Title = "Code ListView";
+        SetDefaultSize(300, 300);
+
+        var stringList = StringList.New(new string[] { "One", "Two", "Three", "Four" });
+        var selectionModel = NoSelection.New(stringList);
+        var listItemFactory = SignalListItemFactory.New();
+        listItemFactory.OnSetup += SetupSignalHandler;
+        listItemFactory.OnBind += BindSignalHandler;
+
+        var listView = ListView.New(selectionModel, listItemFactory);
+
+        var scrolledWindow = ScrolledWindow.New();
+        scrolledWindow.Child = listView;
+        Child = scrolledWindow;
+    }
+
+    private void SetupSignalHandler(SignalListItemFactory sender, SetupSignalArgs args)
+    {
+        var listItem = args.Object as ListItem;
+        if (listItem is null)
+        {
+            return;
+        }
+
+        var label = Label.New(null);
+        listItem.Child = label;
+    }
+
+    private void BindSignalHandler(SignalListItemFactory sender, BindSignalArgs args)
+    {
+        var listItem = args.Object as ListItem;
+        if (listItem is null)
+        {
+            return;
+        }
+
+        var label = listItem.Child as Label;
+        if (label is null)
+        {
+            return;
+        }
+
+        var item = listItem.Item as StringObject;
+        label.SetText(item?.String ?? "NOT FOUND");
+    }
+}
---
+++ src/Samples/Gtk-4.0/ListView/TemplateListViewWindow.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using GLib;
+using GObject;
+using Gtk;
+
+namespace ListViewSample;
+
+public class TemplateListViewWindow : Window
+{
+    public TemplateListViewWindow()
+    {
+        Title = "Template ListView";
+        SetDefaultSize(300, 300);
+
+        var stringList = StringList.New(new string[] { "One", "Two", "Three", "Four" });
+        var selectionModel = SingleSelection.New(stringList);
+        var bytes = Assembly.GetExecutingAssembly()
+            .ReadResourceAsByteArray("ListItemTemplate.ui");
+        var listItemFactory = BuilderListItemFactory.NewFromBytes(null, Bytes.New(bytes));
+        var listView = ListView.New(selectionModel, listItemFactory);
+
+        var scrolledWindow = ScrolledWindow.New();
+        scrolledWindow.Child = listView;
+        Child = scrolledWindow;
+    }
+}
