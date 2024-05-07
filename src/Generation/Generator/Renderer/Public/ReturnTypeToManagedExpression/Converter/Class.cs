0cc65d5ddcb20c6b6262f8d032c844c0e456adfe
badcel <1218031+badcel@users.noreply.github.com>
2024-05-07 17:56:56 +0200

    Fundamental types: Support returning a null value

--- src/Generation/Generator/Renderer/Public/ReturnTypeToManagedExpression/Converter/Class.cs
+++ src/Generation/Generator/Renderer/Public/ReturnTypeToManagedExpression/Converter/Class.cs
@@ -10,14 +10,26 @@ internal class Class : ReturnTypeConverter
 
     public string GetString(GirModel.ReturnType returnType, string fromVariableName)
     {
-        var cls = (GirModel.Class) returnType.AnyType.AsT0;
-
         if (!returnType.IsPointer)
             throw new NotImplementedException($"Can't convert {returnType} to managed as it is a pointer");
 
-        if (cls.Fundamental)
-            return $"new {ComplexType.GetFullyQualified(cls)}({fromVariableName})";
+        var cls = (GirModel.Class) returnType.AnyType.AsT0;
+        return cls.Fundamental
+            ? Fundamental(cls, returnType, fromVariableName)
+            : Standard(cls, returnType, fromVariableName);
+    }
 
+    private static string Fundamental(GirModel.Class cls, GirModel.ReturnType returnType, string fromVariableName)
+    {
+        var ctor = $"new {ComplexType.GetFullyQualified(cls)}({fromVariableName})";
+
+        return returnType.Nullable
+            ? $"{fromVariableName} == IntPtr.Zero ? null : {ctor}"
+            : ctor;
+    }
+
+    private static string Standard(GirModel.Class cls, GirModel.ReturnType returnType, string fromVariableName)
+    {
         return returnType.Nullable
             ? $"GObject.Internal.ObjectWrapper.WrapNullableHandle<{ComplexType.GetFullyQualified(cls)}>({fromVariableName}, {Transfer.IsOwnedRef(returnType.Transfer).ToString().ToLower()})"
             : $"GObject.Internal.ObjectWrapper.WrapHandle<{ComplexType.GetFullyQualified(cls)}>({fromVariableName}, {Transfer.IsOwnedRef(returnType.Transfer).ToString().ToLower()})";
