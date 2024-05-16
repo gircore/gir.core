d27611881bb4ac70dca9863547fe508f94b67946
badcel <1218031+badcel@users.noreply.github.com>
2024-05-16 21:07:20 +0200

    Object: Remove INotifyPropertyChanged interface
    Implementing INotifyPropertyChanged seems not to be helpful currently. The interface was implemented since the very beginning of GirCore but the PropertyChanged event was never raised. As there were no complaints there is probably no need for this interface.
    
    Implementing it would mean that every object has 2 different ways of notifying about property changes (1. Notify event, 2. PropertyChanged) which seems a bit odd in the first place. As the notify event is the native system it should be preferred.
    
    While considering a possible implementation it came to mind that the PropertyChanged event should contain the managed name of the property. This would be the only sensible way for a C# dev, so the instance could potentially be compatible to other frameworks which require an instance of INotifyPropertyChanged and access the property via reflection.
    
    To raise the PropertyChanged event every object would always be required to subscribe to the Notify signal and forward it to PropertyChanged (after some property name mapping happend, which could get non trivial, too). If a user does need the INotifyPropertyChanged interface it could be implemented manually as a subclass and the mapping could be implemented manually.

--- src/Libs/GObject-2.0/Public/Object.cs
+++ src/Libs/GObject-2.0/Public/Object.cs
@@ -8,13 +8,8 @@ using GObject.Internal;
 
 namespace GObject;
 
-public partial class Object : IObject, INotifyPropertyChanged, IDisposable, IHandle
+public partial class Object : IObject, IDisposable, IHandle
 {
-    /// <summary>
-    /// Event triggered when a property value changes.
-    /// </summary>
-    public event PropertyChangedEventHandler? PropertyChanged;
-
     private readonly ObjectHandle _handle;
 
     public IntPtr Handle => _handle.Handle;
@@ -71,18 +66,6 @@ public partial class Object : IObject, INotifyPropertyChanged, IDisposable, IHan
     protected virtual void Initialize()
     {
         Debug.WriteLine($"Initialising Object: Address {_handle.Handle}, Type {GetType()}.");
-    }
-
-    /// <summary>
-    /// Notify this object that a property has just changed.
-    /// </summary>
-    /// <param name="propertyName">The name of the property who changed.</param>
-    protected internal void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-    {
-        if (propertyName == null)
-            return;
-
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
     public virtual void Dispose()
