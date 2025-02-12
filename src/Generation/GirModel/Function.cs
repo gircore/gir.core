ff04e6d5e42d8f3e643c3e11243918d122b125ca
badcel <1218031+badcel@users.noreply.github.com>
2025-02-12 15:29:08 +0100

    Fix code format Building on CI failed due to updated dotnet version which included a newer version of "dotnet format" command.

--- src/Generation/GirModel/Function.cs
+++ src/Generation/GirModel/Function.cs
@@ -2,13 +2,13 @@
 
 public interface Function : Callable
 {
-    public Namespace Namespace { get; }
+    Namespace Namespace { get; }
 
     /// <summary>
     /// The containing type of this function. If null it is a global function.
     /// </summary>
     ComplexType? Parent { get; }
     string CIdentifier { get; }
-    public bool Introspectable { get; }
+    bool Introspectable { get; }
     string? Version { get; }
 }
--- src/Libs/GObject-2.0/Public/PropertyDefinition.cs
+++ src/Libs/GObject-2.0/Public/PropertyDefinition.cs
@@ -5,10 +5,10 @@ public interface PropertyDefinition<T>
     /// <summary>
     /// The name of the property in GObject/C.
     /// </summary>
-    public string UnmanagedName { get; }
+    string UnmanagedName { get; }
 
     /// <summary>
     /// The name of the property in dotnet.
     /// </summary>
-    public string ManagedName { get; }
+    string ManagedName { get; }
 }
