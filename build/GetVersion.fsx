c6e16b50620bdd9dfdddd95e4c5613c7ae34ad53
badcel <1218031+badcel@users.noreply.github.com>
2025-03-05 17:46:10 +0100

    Build: Add new F# script to determine current version number

---
+++ build/GetVersion.fsx
@@ -0,0 +1,14 @@
+#r "nuget: FSharp.Data, 6.4.1"
+
+open FSharp.Data
+
+type publishingProperties = XmlProvider<"../src/Properties/GirCore.Publishing.props">
+
+let versionPrefix = publishingProperties.GetSample().PropertyGroups
+                    |> Seq.tryFind(fun p -> p.VersionPrefix.IsSome)     
+                    |> function
+                        |Some vp -> vp.VersionPrefix.Value
+                        |None -> "Versionprefix not found"
+
+System.Console.WriteLine(versionPrefix)
+            
