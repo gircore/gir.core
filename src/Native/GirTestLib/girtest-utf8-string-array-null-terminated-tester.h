49a9abf74b6364584f0e4cc7ad615872fb4b1ff2
badcel <1218031+badcel@users.noreply.github.com>
2025-03-29 13:44:18 +0100

    Improve support for size based string arrays

--- src/Native/GirTestLib/girtest-utf8-string-array-null-terminated-tester.h
+++ src/Native/GirTestLib/girtest-utf8-string-array-null-terminated-tester.h
@@ -19,6 +19,10 @@ gchar** girtest_utf8_string_array_null_terminated_tester_return_transfer_contain
 gchar** girtest_utf8_string_array_null_terminated_tester_return_transfer_full_nullable(gchar** data);
 gchar** girtest_utf8_string_array_null_terminated_tester_return_transfer_none_nullable(gchar** data);
 gchar** girtest_utf8_string_array_null_terminated_tester_return_transfer_container_nullable(gchar* first, gchar* second);
+gchar** girtest_utf8_string_array_null_terminated_tester_optional_size_out_transfer_full(gchar** data, int *size);
+gchar** girtest_utf8_string_array_null_terminated_tester_size_out_transfer_full(gchar** data, int *size);
+gchar ** girtest_utf8_string_array_null_terminated_tester_optional_gsize_parameter_out_transfer_full(gchar** data, gsize *size);
+gchar ** girtest_utf8_string_array_null_terminated_tester_gsize_parameter_out_transfer_full(gchar** data, gsize *size);
 void girtest_utf8_string_array_null_terminated_tester_parameter_out_transfer_full(gchar** data, gchar*** output);
 void girtest_utf8_string_array_null_terminated_tester_parameter_out_transfer_none(gchar** data, gchar*** output);
 void girtest_utf8_string_array_null_terminated_tester_parameter_out_transfer_container(gchar* first, gchar* second, gchar*** output);
