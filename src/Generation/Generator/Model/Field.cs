36abfeb8dfc1d10781225c261c0c2b368a0095ea
Ferenc Czirok <ferenc@czirok.net>
2025-05-12 20:54:20 +0200

    Add fixer for record-field name collision
    e.g. Length.Length using PropertyNamedLikeRecordFixer

--- src/Generation/Generator/Model/Field.cs
+++ src/Generation/Generator/Model/Field.cs
@@ -1,9 +1,24 @@
+using System.Collections.Generic;
+
 namespace Generator.Model;
 
 internal static class Field
 {
+    private static readonly Dictionary<GirModel.Field, string> FixedNames = new();
+
     public static string GetName(GirModel.Field field)
     {
+        if (FixedNames.TryGetValue(field, out var value))
+            return value;
+
         return field.Name.ToPascalCase();
+    }
+
+    public static void SetName(GirModel.Field field, string name)
+    {
+        lock (FixedNames)
+        {
+            FixedNames[field] = name;
+        }
     }
 }
