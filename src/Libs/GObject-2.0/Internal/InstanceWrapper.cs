96875a45b1d3c194e5019c4a632d4b66db81a16d
ousnius <ousnius@users.noreply.github.com>
2025-09-13 22:37:57 +0200

    Add GObject.Object to generic type constraint of instance factory/wrapper
    Prevents accidental use of DynamicInstanceFactory and InstanceWrapper for types that are not objects/classes, such as records. Records should use BoxedWrapper instead to wrap handles.

--- src/Libs/GObject-2.0/Internal/InstanceWrapper.cs
+++ src/Libs/GObject-2.0/Internal/InstanceWrapper.cs
@@ -4,14 +4,14 @@ namespace GObject.Internal;
 
 public static class InstanceWrapper
 {
-    public static object? WrapNullableHandle<TFallback>(IntPtr handle, bool ownedRef) where TFallback : InstanceFactory, GTypeProvider
+    public static object? WrapNullableHandle<TFallback>(IntPtr handle, bool ownedRef) where TFallback : GObject.Object, InstanceFactory, GTypeProvider
     {
         return handle == IntPtr.Zero
             ? null
             : WrapHandle<TFallback>(handle, ownedRef);
     }
 
-    public static object WrapHandle<TFallback>(IntPtr handle, bool ownedRef) where TFallback : InstanceFactory, GTypeProvider
+    public static object WrapHandle<TFallback>(IntPtr handle, bool ownedRef) where TFallback : GObject.Object, InstanceFactory, GTypeProvider
     {
         if (handle == IntPtr.Zero)
             throw new NullReferenceException("Failed to wrap handle: Null handle passed to WrapHandle.");
