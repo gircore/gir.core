8c0c91b7fd77375cb30e11f93669f34ba57e04cc
ousnius <ousnius@users.noreply.github.com>
2025-09-20 19:57:34 +0200

    Add pango CSS scale factor constants
    See header pango-font.h
    
    * Pango.Constants.XX_SMALL
    * Pango.Constants.X_SMALL
    * Pango.Constants.SMALL
    * Pango.Constants.MEDIUM
    * Pango.Constants.LARGE
    * Pango.Constants.X_LARGE
    * Pango.Constants.XX_LARGE

---
+++ src/Libs/Pango-1.0/Public/Constants.cs
@@ -0,0 +1,39 @@
+﻿namespace Pango;
+
+public partial class Constants
+{
+    /// <summary>
+    /// The CSS scale factor for three shrinking steps (1 / (1.2 * 1.2 * 1.2)).
+    /// </summary>
+    public const double XX_SMALL = 0.5787037037037;
+
+    /// <summary>
+    /// The scale factor for two shrinking steps (1 / (1.2 * 1.2)).
+    /// </summary>
+    public const double X_SMALL = 0.6944444444444;
+
+    /// <summary>
+    /// The scale factor for one shrinking step (1 / 1.2).
+    /// </summary>
+    public const double SMALL = 0.8333333333333;
+
+    /// <summary>
+    /// The scale factor for normal size (1.0).
+    /// </summary>
+    public const double MEDIUM = 1.0;
+
+    /// <summary>
+    /// The scale factor for one magnification step (1.2).
+    /// </summary>
+    public const double LARGE = 1.2;
+
+    /// <summary>
+    /// The scale factor for two magnification steps (1.2 * 1.2).
+    /// </summary>
+    public const double X_LARGE = 1.44;
+
+    /// <summary>
+    /// The scale factor for three magnification steps (1.2 * 1.2 * 1.2).
+    /// </summary>
+    public const double XX_LARGE = 1.728;
+}
