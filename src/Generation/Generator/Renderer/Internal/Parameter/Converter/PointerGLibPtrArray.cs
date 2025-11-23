b2d4d04306c401b23547325b420b6df94ead311e
Alan Sartorio <alan42ga@hotmail.com>
2025-11-23 20:50:03 -0300

    Use handles instead of IntPtr for internal PtrArray API

--- src/Generation/Generator/Renderer/Internal/Parameter/Converter/PointerGLibPtrArray.cs
+++ src/Generation/Generator/Renderer/Internal/Parameter/Converter/PointerGLibPtrArray.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Generator.Renderer.Internal.Parameter;
 
 public class PointerGLibPtrArray : ParameterConverter
@@ -11,10 +13,26 @@ public class PointerGLibPtrArray : ParameterConverter
     {
         return new RenderableParameter(
             Attribute: string.Empty,
-            Direction: string.Empty,
-            NullableTypeName: Model.Type.Pointer,
+            Direction: GetDirection(parameter),
+            NullableTypeName: GetNullableTypeName(parameter),
             Name: Model.Parameter.GetName(parameter)
         );
     }
+
+    private static string GetNullableTypeName(GirModel.Parameter parameter)
+    {
+        return parameter switch
+        {
+            { Direction: GirModel.Direction.In, Transfer: GirModel.Transfer.None } => Model.PointerArrayType.GetFullyQuallifiedHandle(),
+            { Direction: GirModel.Direction.In, Transfer: GirModel.Transfer.Full } => Model.PointerArrayType.GetFullyQuallifiedUnownedHandle(),
+            _ => throw new Exception($"Can't detect ptrarray parameter type {parameter.Name}: CallerAllocates={parameter.CallerAllocates} Direction={parameter.Direction} Transfer={parameter.Transfer}")
+        };
+    }
+
+    private static string GetDirection(GirModel.Parameter parameter) => parameter switch
+    {
+        { Direction: GirModel.Direction.In } => ParameterDirection.In(),
+        _ => throw new Exception($"Unknown parameter direction for ptrarray parameter {parameter.Name}")
+    };
 }
 
--- src/Generation/Generator/Renderer/Public/ParameterToNativeExpression/Converter/GLibPointerArray.cs
+++ src/Generation/Generator/Renderer/Public/ParameterToNativeExpression/Converter/GLibPointerArray.cs
@@ -15,8 +15,20 @@ internal class GLibPointerArray : ToNativeParameterConverter
         if (!parameter.Parameter.IsPointer)
             throw new NotImplementedException($"{parameter.Parameter.AnyTypeOrVarArgs}: non pointer ptrarray parameter '{parameter.Parameter.Name}' not yet supported");
 
-        var parameterName = Model.Parameter.GetName(parameter.Parameter);
-        parameter.SetSignatureName(() => parameterName);
-        parameter.SetCallName(() => $"{parameterName}.Handle.DangerousGetHandle()");
+        var typeHandle = Model.PointerArrayType.GetFullyQuallifiedHandle();
+        var nullHandle = Model.PointerArrayType.GetFullyQuallifiedNullHandle();
+        var signatureName = Model.Parameter.GetName(parameter.Parameter);
+
+        var callName = parameter.Parameter switch
+        {
+            { Nullable: true, Transfer: GirModel.Transfer.None } => $"({typeHandle}?) {signatureName}?.Handle ?? {nullHandle}",
+            { Nullable: false, Transfer: GirModel.Transfer.None } => $"{signatureName}.Handle",
+            { Nullable: true, Transfer: GirModel.Transfer.Full } => $"{signatureName}?.Handle.UnownedCopy() ?? {nullHandle}",
+            { Nullable: false, Transfer: GirModel.Transfer.Full } => $"{signatureName}.Handle.UnownedCopy()",
+            _ => throw new Exception($"Can't detect call name for ptrarray parameter {parameter.Parameter.Name}")
+        };
+
+        parameter.SetSignatureName(() => signatureName);
+        parameter.SetCallName(() => callName);
     }
 }
