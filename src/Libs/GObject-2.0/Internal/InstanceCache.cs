239d452560eadf07181024149a402a260d82ff92
badcel <1218031+badcel@users.noreply.github.com>
2026-01-17 23:45:18 +0100

    Fix crash

--- src/Libs/GObject-2.0/Internal/InstanceCache.cs
+++ src/Libs/GObject-2.0/Internal/InstanceCache.cs
@@ -8,7 +8,7 @@ namespace GObject.Internal;
 
 internal static class InstanceCache
 {
-    private static readonly object Lock = new();
+    internal static readonly object Lock = new();
     private static readonly Dictionary<IntPtr, ToggleRef> Cache = new();
     internal static int ObjectCount
     {
@@ -29,7 +29,7 @@ internal static class InstanceCache
             {
                 if (toggleRef.Object is not null)
                 {
-                    obj = toggleRef.Object;
+                    obj = toggleRef.Object.Instance ?? throw new Exception($"Handle {handle} misses a managed counterpart.");
                     return true;
                 }
             }
@@ -39,46 +39,61 @@ internal static class InstanceCache
         return false;
     }
 
-    public static unsafe void Add(IntPtr handle, GObject.Object obj)
+    public static unsafe void Add(ObjectHandle handle)
     {
-        lock (Cache)
+        var ptr = handle.DangerousGetHandle();
+
+        lock (Lock)
         {
-            Cache[handle] = new ToggleRef(obj);
-            ToggleRegistration.AddToggleRef(handle, &ToggleNotify);
-            Object.Unref(handle);
+            // Create cache entry immediately so ToggleNotify can find it even if native add is delayed
+            Cache[ptr] = new ToggleRef(handle);
         }
 
-        Debug.WriteLine($"Handle {handle}: Added object of type '{obj.GetType()}' to {nameof(InstanceCache)}");
+        // Schedule native registration and Unref on the GLib main context (async)
+        var addSource = new GLib.Internal.SourceFuncAsyncHandler(() =>
+        {
+            ToggleRegistration.AddToggleRef(ptr, &ToggleNotify);
+            Object.Unref(ptr);
+
+            return false; // one-shot
+        });
+
+        GLib.Internal.MainContext.Invoke(GLib.Internal.MainContextUnownedHandle.NullHandle, addSource.NativeCallback, IntPtr.Zero);
+
+        Console.WriteLine($"Handle {handle}: Added object of type '{handle.Instance?.GetType()}' to {nameof(InstanceCache)}");
     }
 
     public static unsafe void Remove(IntPtr handle)
     {
-        lock (Cache)
+        // Schedule native removal on the main context. After the native removal is executed
+        // remove the cache entry under the InstanceCache lock to preserve ordering.
+        var removeSource = new GLib.Internal.SourceFuncAsyncHandler(() =>
         {
-            if (Cache.Remove(handle))
-                ToggleRegistration.RemoveToggleRef(handle, &ToggleNotify);
-        }
+            ToggleRegistration.RemoveToggleRef(handle, &ToggleNotify);
 
-        Debug.WriteLine($"Handle {handle}: Removed object from {nameof(InstanceCache)}.");
+            lock (Lock)
+            {
+                Cache.Remove(handle);
+            }
+
+            return false;
+        });
+
+        GLib.Internal.MainContext.Invoke(GLib.Internal.MainContextUnownedHandle.NullHandle, removeSource.NativeCallback, IntPtr.Zero);
+
+        Console.WriteLine($"Handle {handle}: Removed object from {nameof(InstanceCache)}.");
     }
 
     [UnmanagedCallersOnly]
     private static void ToggleNotify(IntPtr data, IntPtr @object, int isLastRef)
     {
-        try
+        lock (Lock)
         {
-            lock (Lock)
-            {
-                if (Cache.TryGetValue(@object, out var toggleRef))
-                    toggleRef.ToggleReference(isLastRef != 0);
-                else
-                    Debug.WriteLine($"Handle {@object}: Could not toggle to {isLastRef} as there is no toggle reference.");
-            }
-        }
-        catch
-        {
-            Debug.WriteLine($"Failed to toggle reference: Object={@object}, type={TypeNameFromInstance(@object).ConvertToString()}, isLastRef={isLastRef}.");
-            throw;
+            Console.WriteLine($"Toggle reference from handle {@object}, is last ref: {isLastRef}.");
+            if (Cache.TryGetValue(@object, out var toggleRef))
+                toggleRef.ToggleReference(isLastRef != 0);
+            else
+                Console.WriteLine($"Handle {@object}: Could not toggle to {isLastRef} as there is no toggle reference.");
         }
     }
 
--- src/Libs/GObject-2.0/Internal/ToggleRef.cs
+++ src/Libs/GObject-2.0/Internal/ToggleRef.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 namespace GObject.Internal;
 
@@ -6,41 +7,47 @@ internal class ToggleRef
 {
     private object _reference;
 
-    public GObject.Object? Object
+    public ObjectHandle? Object
     {
         get
         {
-            if (_reference is WeakReference weakRef)
-                return (GObject.Object?) weakRef.Target;
+            lock (InstanceCache.Lock)
+            {
+                if (_reference is WeakReference weakRef)
+                    return (ObjectHandle?) weakRef.Target;
 
-            return (GObject.Object) _reference;
+                return (ObjectHandle) _reference;
+            }
         }
     }
 
     /// <summary>
-    /// This object saves a strong reference to the given object which prevents it from beeing garbage
+    /// This object saves a strong reference to the given object handle which prevents it from being garbage
     /// collected. This strong reference is hold as long as there are other references than the toggling ref
     /// on the given object.
-    /// If the toggeling ref is the last ref on the given object the strong reference is changed into a
+    /// If the toggeling ref is the last ref on the given object handle the strong reference is changed into a
     /// weak reference. This is signaled via a call to "ToggleReference".
     /// </summary>
-    public ToggleRef(GObject.Object obj)
+    public ToggleRef(ObjectHandle obj)
     {
         _reference = obj;
     }
 
     internal void ToggleReference(bool isLastRef)
     {
-        if (!isLastRef && _reference is WeakReference weakRef)
+        lock (InstanceCache.Lock)
         {
-            if (weakRef.Target is { } weakObj)
-                _reference = weakObj;
-            else
-                throw new Exception("Could not toggle reference to strong. It got garbage collected.");
-        }
-        else if (isLastRef && _reference is not WeakReference)
-        {
-            _reference = new WeakReference(_reference);
+            if (!isLastRef && _reference is WeakReference weakRef)
+            {
+                if (weakRef.Target is { } weakObj)
+                    _reference = weakObj;
+                else
+                    Console.WriteLine("Could not create a strong ref. The target got already garbage collected.");
+            }
+            else if (isLastRef && _reference is not WeakReference)
+            {
+                _reference = new WeakReference(_reference);
+            }
         }
     }
 }
