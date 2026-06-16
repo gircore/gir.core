10970ccb4da8d042033285507e12e12ecab22035
badcel <1218031+badcel@users.noreply.github.com>
2026-06-16 21:41:42 +0200

    Tests: Add tests that confirm that namespace collisions do not impact source generators

---
+++ src/Tests/Data/Gtk-4.0.Integration.Tests.Data/NamespaceTest.cs
@@ -0,0 +1,7 @@
+namespace MyNamespace.Gtk
+{
+    [global::GObject.Subclass<global::Gtk.Widget>]
+    [global::Gtk.Template<global::Gtk.AssemblyResource>("test")]
+    public partial class NamespaceSubClass;
+}
+
