5944a58b947109a783a33f6e3ca0ebc3ddc808f0
badcel <1218031+badcel@users.noreply.github.com>
2026-06-18 21:10:10 +0200

    Update

--- src/Samples/Gtk-4.0/TreeExpanderTest2/Program.cs
+++ src/Samples/Gtk-4.0/TreeExpanderTest2/Program.cs
@@ -5,17 +5,34 @@ namespace Program;
 
 class Program
 {
-    private static readonly Application BasicApp = Application.New("ua.org.accounting.test", Gio.ApplicationFlags.FlagsNone);
-
+    public static Gio.ListStore Store { get; } = Gio.ListStore.New(GObject.Object.GetGType());
+    
     private static void Main()
     {
-        BasicApp.OnActivate += (app, args) =>
+        var app = Application.New("ua.org.accounting.test", Gio.ApplicationFlags.FlagsNone);
+        app.OnActivate += (_, _) =>
         {
-            var window = FormWindow.New();
-            window.Application = BasicApp;
+            var button = Button.NewWithLabel("Add");
+            button.OnClicked +=  (_, _) => Add();
+            
+            var window = Gtk.Window.New();
+            window.SetDefaultSize(100, 100);
+            window.Application = app;
+            window.Child = button;
             window.Show();
         };
 
-        BasicApp.RunWithSynchronizationContext(null);
+        app.RunWithSynchronizationContext(null);
+    }
+    
+    static void Add()
+    {
+        MultiSelection model1 = MultiSelection.New(Store);
+        MultiSelection model3 = MultiSelection.New(Store);
+        
+        for (int i = 0; i < 5000; i++)
+        {
+            Store.Append(GObject.Object.NewWithProperties([]));
+        }
     }
 }
